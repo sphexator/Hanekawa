d047b2ba9141f0ff164202137322c891f850f638
Sphexator <kristoffer.tunlid@gmail.com>
2017-10-06 13:13:22 +0200

    Add project files.

---
+++ Jibril/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jibril
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
