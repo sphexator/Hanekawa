74c2c0e8ff4a9afd1b8c05d8b8e75c060e28c626
Sphexator <kristoffer.tunlid@gmail.com>
2017-10-06 14:41:37 +0200

    Package setup

--- Jibril/Program.cs
+++ Jibril/Program.cs
@@ -2,9 +2,14 @@
 
 namespace Jibril
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
+            => new Program().MainASync().GetAwaiter().GetResult();
+        private DiscordSocketClient _client;
+        private IConfiguration _config;
+
+        public async Task MainASync()
         {
 
         }
