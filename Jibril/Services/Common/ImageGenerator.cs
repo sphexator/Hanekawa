b46f901209ca96f70d5ffffa933cea68cb8b9f44
Sphexator <kristoffer.tunlid@gmail.com>
2017-10-09 11:49:47 +0200

    Welcomer alpha stage

--- Jibril/Services/Common/ImageGenerator.cs
+++ Jibril/Services/Common/ImageGenerator.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Discord.WebSocket;
+using SixLabors.ImageSharp;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Jibril.Services.Common
 {
@@ -9,6 +14,27 @@ namespace Jibril.Services.Common
         public static void RandomBackground()
         {
 
+        }
+
+        public static async Task<string> AvatarGenerator(SocketUser user, string randomString)
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = null;
+            var aviPath = $"Data/Welcome/Cache/Avatar/{randomString}.jpg";
+            try
+            {
+                response = await httpClient.GetAsync(user.GetAvatarUrl());
+            }
+            catch
+            {
+                response = await httpClient.GetAsync("https://discordapp.com/assets/1cbd08c76f8af6dddce02c5138971129.png");
+            }
+            Stream inputStream = await response.Content.ReadAsStreamAsync();
+            using (var img = Image.Load(inputStream))
+            {
+                img.Save(aviPath);
+            }
+            return aviPath;
         }
     }
 }
--- Jibril/Services/Welcome/Services/WelcImgGen.cs
+++ Jibril/Services/Welcome/Services/WelcImgGen.cs
@@ -1,22 +1,25 @@
 ﻿using Discord.WebSocket;
 using Jibril.Data.Variables;
 using Jibril.Services.Common;
+using SixLabors.Fonts;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing;
 using SixLabors.Primitives;
 using SixLabors.Shapes;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Jibril.Services.Welcome.Services
 {
     public class WelcImgGen
     {
-        public static void WelcomeImageGenerator(SocketGuildUser user)
+        public static async Task WelcomeImageGeneratorAsync(SocketGuildUser user)
         {
             var randomString = RandomStringGenerator.StringGenerator();
-
+            var avatarToLoad = await ImageGenerator.AvatarGenerator(user, randomString);
             Random rand = new Random();
             var randomImage = rand.Next(Images.Welcome.Length);
             var filetoLoad = Images.Welcome[randomImage];
@@ -24,12 +27,24 @@ namespace Jibril.Services.Welcome.Services
             using (var img = Image.Load(filetoLoad))
             {
                 PathBuilder pathBuilder = new PathBuilder();
-                pathBuilder.SetOrigin(new PointF(95,46));
-                pathBuilder.AddBezier(new PointF());
-                // bottom height is         96
-                // top part is              46
-                // Start of the name is at  95
-                // End of the name is at    388
+                pathBuilder.SetOrigin(new PointF(95, 46));
+                pathBuilder.AddBezier(new PointF(95, 46), new PointF(241, 71), new PointF(388, 96));
+                // bottom Height            96
+                // top Height               46
+                // Start Width              95
+                // End Width                388
+
+                IPath path = pathBuilder.Build();
+                var avatar = Image.Load(avatarToLoad);
+                var font = SystemFonts.CreateFont("Times New Roman", 34, FontStyle.Regular);
+                var text = user.Username;
+                img.Mutate(ctx => ctx
+                    .DrawText(text, font, Rgba32.White, path, new TextGraphicsOptions(true) // draw the text along the path wrapping at the end of the line
+                    {
+                        WrapTextWidth = path.Length
+                    }));
+                img.Mutate(ctx => ctx.DrawImage(avatar, new Size(80, 80), new Point(10, 10), GraphicsOptions.Default));
+                img.Save($"Data/Welcome/Cache/Banner/{randomString}.png");
             }
         }
     }
