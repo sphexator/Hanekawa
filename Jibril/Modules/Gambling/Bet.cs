0246227f0508b81a55ec8095e4e0cd37136715ae
Sphexator <kristoffer.tunlid@gmail.com>
2017-10-10 13:25:14 +0200

    [NEW]Custom preconditions added

---
+++ Jibril/Modules/Gambling/Bet.cs
@@ -0,0 +1,22 @@
+﻿using Discord.Addons.Preconditions;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jibril.Modules.Gambling
+{
+    public class Bet
+    {
+        public static TimeSpan span = new TimeSpan(0, 0, 05);
+        [Command("bet")]
+        [RequireContext(ContextType.Guild)]
+        [Ratelimit(12, 1, Measure.Minutes, false, false)]
+        public async Task HardBet(uint bet)
+        {
+
+        }
+
+    }
+}
---
+++ Jibril/Preconditions/RequiredChannel.cs
@@ -0,0 +1,34 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jibril.Preconditions
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class RequiredChannel : RequireContextAttribute
+    {
+        private readonly ulong _requiredChannel;
+
+        public RequiredChannel(ulong requiredChannel) : base(ContextType.Guild)
+        {
+            _requiredChannel = requiredChannel;
+        }
+
+        public async override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            var baseResult = await base.CheckPermissions(context, command, services);
+            if (baseResult.IsSuccess && ((IGuildChannel)context.Channel).Id == _requiredChannel)
+            {
+                return PreconditionResult.FromSuccess();
+            }
+            else
+            {
+                return baseResult;
+            }
+        }
+    }
+}
