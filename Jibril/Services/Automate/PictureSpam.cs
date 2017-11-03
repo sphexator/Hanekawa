4877c4343d3382adeffb0dd162bf1e05d033a08f
Sphexator <kristoffer.tunlid@gmail.com>
2017-11-03 16:00:23 +0100

    Scrapped dropbox idea, looking into google api instead

--- Jibril/Services/Automate/PictureSpam.cs
+++ Jibril/Services/Automate/PictureSpam.cs
@@ -10,31 +10,6 @@ namespace Jibril.Services.Automate
 {
     public class PictureSpam
     {
-        private readonly DiscordSocketClient _discord;
-        private const string ApiKey = "jkjl5abxifozpz4";
 
-        public PictureSpam(DiscordSocketClient discord)
-        {
-            _discord = discord;
-        }
-
-        private Task PostImages()
-        {
-            var _ = Task.Run(async () => 
-            {
-                var client = new DropboxClient(ApiKey);
-                var guild = _discord.GetGuild(234505708861652993);
-                var Dropboxfolder = "KanColle-bot";
-                var output = "Data/Images/PictureSpam/";
-                var ch = guild.TextChannels.FirstOrDefault(x => x.Id == 360140270605434882);
-                var list = await client.Files.ListFolderAsync(Dropboxfolder);
-                foreach(var item in list.Entries.Where(x => x.IsFile))
-                {
-                    await client.Files.DownloadAsync(output + item.Name);
-                    await ch.SendFileAsync(output + item.Name, "");
-                }
-            });
-            return Task.CompletedTask;
-        }
     }
 }
