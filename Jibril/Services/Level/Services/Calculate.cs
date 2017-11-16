8a9562aefe04831345e842bdddc65a6065c922f0
Sphexator <kristoffer.tunlid@gmail.com>
2017-11-16 23:24:37 +0100

    Revamped exp + credit gain calculations

--- Jibril/Services/Level/Services/Calculate.cs
+++ Jibril/Services/Level/Services/Calculate.cs
@@ -8,14 +8,9 @@ namespace Jibril.Services.Level.Services
     {
         public static int ReturnXP(SocketMessage msg)
         {
-            var rand = new Random();
-            var defxp = rand.Next(10, 20);
-            if (msg.Channel.Id == 339383206669320192 || msg.Channel.Id == 346429281314013184)
-            {
-                var xp = defxp / 5;
-                return xp;
-            }
-            return defxp;
+            var xp = CalculateExperience(msg);
+            var returnExp = xp * 2;
+            return returnExp;
         }
 
         public static int CalculateNextLevel(int currentLevel)
@@ -26,13 +21,36 @@ namespace Jibril.Services.Level.Services
 
         public static int ReturnCredit()
         {
-            var rand = new Random();
-            var credit = rand.Next(1, 3);
+            var credit = CalculateCredit();
             return credit;
         }
 
         //Voice Experience credit calculations = VECC
         public static void VECC(IUser user, DateTime vcTimer)
+        {
+            var calculateXp = CalculateVoiceExperience(vcTimer) * 2;
+            var calculateCredit = CalculateVoiceCredit(vcTimer);
+
+            if (calculateXp > 0)
+                LevelDatabase.AddExperience(user, calculateXp, calculateCredit);
+        }
+
+        private static int CalculateExperience(SocketMessage msg)
+        {
+            var rand = new Random();
+            var xp = rand.Next(10, 20);
+            if (msg.Channel.Id == 339383206669320192 || msg.Channel.Id == 346429281314013184) return xp / 5;
+            return xp;
+        }
+
+        private static int CalculateCredit()
+        {
+            var rand = new Random();
+            var credit = rand.Next(1, 3);
+            return credit;
+        }
+
+        private static int CalculateVoiceExperience(DateTime vcTimer)
         {
             var now = DateTime.Now;
 
@@ -41,13 +59,20 @@ namespace Jibril.Services.Level.Services
             var minutes = int.Parse(diff.Minutes.ToString());
             var totalTime = hours * 60 + minutes;
 
-            var CalculateXP = totalTime * 2;
-            var CalculateCredit = totalTime;
+            var calculateXp = totalTime * 2;
+            return calculateXp;
+        }
 
-            if (CalculateXP > 0)
-            {
-                LevelDatabase.AddExperience(user, CalculateXP, CalculateCredit);
-            }
+        private static int CalculateVoiceCredit(DateTime vcTimer)
+        {
+            var now = DateTime.Now;
+
+            var diff = now - vcTimer;
+            var hours = int.Parse(diff.Hours.ToString());
+            var minutes = int.Parse(diff.Minutes.ToString());
+            var totalTime = hours * 60 + minutes;
+
+            return totalTime;
         }
     }
 }
