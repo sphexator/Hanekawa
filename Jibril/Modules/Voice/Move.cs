feab5614b026971bb8025933ecc8597f9736f0f0
Sphexator <kristoffer.tunlid@gmail.com>
2017-11-27 17:05:40 +0100

    Today my bike was stolen

---
+++ Jibril/Modules/Voice/Move.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Addons.Interactive;
+using Discord.Commands;
+using Discord.WebSocket;
+using Jibril.Preconditions;
+
+namespace Jibril.Modules.Voice
+{
+    public class Move : InteractiveBase
+    {
+        [Command("move", RunMode = RunMode.Async)]
+        [UserMustBeInVoice]
+        [RequireContext(ContextType.Guild)]
+        [Ratelimit(1, 2, Measure.Seconds)]
+        public async Task MoveUser(SocketGuildUser user)
+        {
+            
+        }
+    }
+}
---
+++ Jibril/Preconditions/UserMustBeInVoiceAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord;
+
+namespace Jibril.Preconditions
+{
+    /// <summary> Indicates that this command should only be used while the user is in a voice channel. </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public sealed class UserMustBeInVoiceAttribute : PreconditionAttribute
+    {
+        /// <inheritdoc />
+        public override async Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            var current = (context.User as IVoiceState)?.VoiceChannel?.Id;
+            return (await context.Guild.GetVoiceChannelsAsync()).Any(v => v.Id != current)
+                ? PreconditionResult.FromError("Command must be invoked while in a voice channel in this guild.")
+                : PreconditionResult.FromSuccess();
+        }
+    }
+}
