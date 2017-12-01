a61b4ba70d21c8cc7c3927eeea9ab9153a25fcb1
Sphexator <kristoffer.tunlid@gmail.com>
2017-12-01 22:44:16 +0100

    buggu

--- Jibril/Modules/Voice/Move.cs
+++ Jibril/Modules/Voice/Move.cs
@@ -23,27 +23,33 @@ namespace Jibril.Modules.Voice
         [Ratelimit(1, 2, Measure.Seconds)]
         public async Task MoveUser(SocketGuildUser user)
         {
-            if ((Context.User as IVoiceState) == null) return;
-            var vcUsers = await (Context.User as IVoiceState).VoiceChannel.GetUsersAsync().ToList();
-            var users = new List<UserData>();
-            foreach (var vcu in vcUsers)
+            try
             {
-                var dataUser = DatabaseService.UserData((vcu as IUser));
-                users.AddRange(dataUser);
-            }
-            var mu = users.OrderByDescending(x => x.Voice_timer).FirstOrDefault();
-            var mui = Convert.ToUInt64(mu.UserId);
-            if (mui == Context.User.Id)
-            {
-                await ReplyAsync($"{Context.User.Mention} wants to move {user.Mention}, do you accept? (Y/N)");
-                var response = await NextMessageAsync(new EnsureFromUserCriterion(user.Id));
-                if (response.Content.Equals("Y", StringComparison.InvariantCultureIgnoreCase))
+                var vcUsers = await (Context.User as IVoiceState).VoiceChannel.GetUsersAsync().ToList();
+                var users = new List<UserData>();
+                foreach (var vcu in vcUsers)
                 {
-                    await user.ModifyAsync(x => x.ChannelId = (Context.User as IVoiceState).VoiceChannel.Id);
-                    await ReplyAsync($"Moved {user.Username} to {Context.User.Username} voice channel");
+                    var dataUser = DatabaseService.UserData((vcu as IUser));
+                    users.AddRange(dataUser);
                 }
+                var mu = users.OrderByDescending(x => x.Voice_timer).FirstOrDefault();
+                var mui = Convert.ToUInt64(mu.UserId);
+                if (mui == Context.User.Id)
+                {
+                    await ReplyAsync($"{Context.User.Mention} wants to move {user.Mention}, do you accept? (Y/N)");
+                    var response = await NextMessageAsync(new EnsureFromUserCriterion(user.Id));
+                    if (response.Content.Equals("Y", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        await user.ModifyAsync(x => x.ChannelId = (Context.User as IVoiceState).VoiceChannel.Id);
+                        await ReplyAsync($"Moved {user.Username} to {Context.User.Username} voice channel");
+                    }
+                }
+                else await ReplyAsync($"You cannot use this command. Ask <@{mui}> instead.");
             }
-            else await ReplyAsync($"You cannot use this command. Ask <@{mui}> instead.");
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
