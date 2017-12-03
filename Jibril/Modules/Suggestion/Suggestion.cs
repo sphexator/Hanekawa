4d4c099d7f0204ca6b07513c23e16e243b1cfefe
Sphexator <kristoffer.tunlid@gmail.com>
2017-12-03 08:45:08 +0100

    bubblegum

--- Jibril/Modules/Suggestion/Suggestion.cs
+++ Jibril/Modules/Suggestion/Suggestion.cs
@@ -20,51 +20,61 @@ namespace Jibril.Modules.Suggestion
         [Ratelimit(1, 5, Measure.Seconds)]
         public async Task ServerSuggestiong([Remainder] string content = null)
         {
-            var confirm = EmbedGenerator.DefaultEmbed($"Suggestion sent to server requests", Colours.OKColour);
-            await ReplyAndDeleteAsync("", false, confirm.Build(), TimeSpan.FromSeconds(15));
-            await Context.Message.DeleteAsync();
-
-            var time = DateTime.Now;
-            var guild = Context.Guild;
-            var sc = guild.GetTextChannel(342519715215835136);
-
-            await Task.Delay(100);
-
-            SuggestionDB.AddSuggestion(Context.User, time);
-            var suggestionNr = SuggestionDB.GetSuggestionID(time);
-
-
-            var author = new EmbedAuthorBuilder();
-            var footer = new EmbedFooterBuilder();
-            var embed = new EmbedBuilder
-            {
-                Color = new Color(Colours.DefaultColour),
-                Description = $"{content}"
-            };
-            if (Context.Message.Attachments.ToString() == null) embed.ImageUrl = Context.Message.Attachments.ToString();
-
-            author.WithIconUrl(Context.User.GetAvatarUrl());
-            author.WithName(Context.User.Username);
-
-            footer.WithText($"Suggestion ID: {suggestionNr[0]}");
-
-            embed.WithAuthor(author);
-            embed.WithFooter(footer);
-
             try
             {
-                var suggestMsg = await sc.SendMessageAsync("", false, embed.Build());
-                SuggestionDB.UpdateSuggestion(suggestMsg.Id.ToString(), suggestionNr[0]);
 
-                await Task.Delay(260);
-                await suggestMsg.AddReactionAsync(new Emoji("👍"));
-                await Task.Delay(260);
-                await suggestMsg.AddReactionAsync(new Emoji("👎"));
+
+                var confirm = EmbedGenerator.DefaultEmbed($"Suggestion sent to server requests", Colours.OKColour);
+                await ReplyAndDeleteAsync("", false, confirm.Build(), TimeSpan.FromSeconds(15));
+                await Context.Message.DeleteAsync();
+
+                var time = DateTime.Now;
+                var guild = Context.Guild;
+                var sc = guild.GetTextChannel(342519715215835136);
+
+                await Task.Delay(100);
+
+                SuggestionDB.AddSuggestion(Context.User, time);
+                var suggestionNr = SuggestionDB.GetSuggestionID(time);
+
+
+                var author = new EmbedAuthorBuilder();
+                var footer = new EmbedFooterBuilder();
+                var embed = new EmbedBuilder
+                {
+                    Color = new Color(Colours.DefaultColour),
+                    Description = $"{content}"
+                };
+                if (Context.Message.Attachments.ToString() == null)
+                    embed.ImageUrl = Context.Message.Attachments.ToString();
+
+                author.WithIconUrl(Context.User.GetAvatarUrl());
+                author.WithName(Context.User.Username);
+
+                footer.WithText($"Suggestion ID: {suggestionNr[0]}");
+
+                embed.WithAuthor(author);
+                embed.WithFooter(footer);
+
+                try
+                {
+                    var suggestMsg = await sc.SendMessageAsync("", false, embed.Build());
+                    SuggestionDB.UpdateSuggestion(suggestMsg.Id.ToString(), suggestionNr[0]);
+
+                    await Task.Delay(260);
+                    await suggestMsg.AddReactionAsync(new Emoji("👍"));
+                    await Task.Delay(260);
+                    await suggestMsg.AddReactionAsync(new Emoji("👎"));
+                }
+                catch (Exception e)
+                {
+                    Console.Write(e);
+                    await ReplyAsync("Something went wrong");
+                }
             }
             catch (Exception e)
             {
-                Console.Write(e);
-                await ReplyAsync("Something went wrong");
+                Console.WriteLine(e);
             }
         }
 
