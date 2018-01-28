70843cc4179e06d3914da2cbfb0d7dc299b612c0
Sphexator <kristoffer.tunlid@gmail.com>
2018-01-28 19:38:50 +0100

    Reliability service added

---
+++ Jibril/Services/Interactivity/I-am-infamous.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace Jibril.Services.Interactivity
+{
+    public class I_am_infamous
+    {
+        private readonly DiscordSocketClient _client;
+
+        public I_am_infamous(DiscordSocketClient client)
+        {
+            _client = client;
+
+            _client.MessageReceived += MessageCounter;
+        }
+
+        private Task MessageCounter(SocketMessage arg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Jibril/Services/Loot/LootCrates.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace Jibril.Services.Loot
+{
+    public class LootCrates
+    {
+        private readonly DiscordSocketClient _discord;
+
+        public LootCrates(DiscordSocketClient discord)
+        {
+            _discord = discord;
+
+            _discord.MessageReceived += CrateTrigger;
+        }
+
+        private Task CrateTrigger(SocketMessage msg)
+        {
+            var _ = Task.Run(async () =>
+            {
+                if (!(msg is SocketUserMessage message)) return;
+                if (message.Source != MessageSource.User) return;
+
+                var rand = new Random();
+                var chance = rand.Next(0, 100);
+                if (chance < 20)
+                {
+
+                }
+            });
+            return Task.CompletedTask;
+        }
+    }
+}
