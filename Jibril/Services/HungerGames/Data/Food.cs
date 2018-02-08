01d18354f8930902b4ef3cefc23b39c28b658b12
Sphexator <kristoffer.tunlid@gmail.com>
2018-02-08 03:52:10 +0100

    Food specific stats added

---
+++ Jibril/Services/HungerGames/Data/Food.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jibril.Services.HungerGames.Data
+{
+    public class Ramen
+    {
+        public int StaminaGain = 30;
+    }
+}
