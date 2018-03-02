1b33bd704c6fdba86bd23bece87bc09cb47b11f6
Sphexator <kristoffer.tunlid@gmail.com>
2018-03-02 20:16:39 +0100

    Added requirement to use commands

--- Jibril/Modules/Report/Report.cs
+++ Jibril/Modules/Report/Report.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Discord.Addons.Interactive;
+﻿using Discord.Addons.Interactive;
+using Discord.Commands;
+using Jibril.Services;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Jibril.Modules.Report
 {
     public class Report : InteractiveBase
     {
+        [Command("report", RunMode = RunMode.Async)]
+        [RequireContext(ContextType.Guild)]
+        public async Task ReportGuild([Remainder] string text)
+        {
+            var userdata = DatabaseService.UserData(Context.User).FirstOrDefault();
+            if (userdata.Level <= 10) await ReplyAsync("You need to be level 10 or above to use the report system.");
 
+            await ReplyAsync("Report sent.");
+        }
+
+        [Command("report", RunMode = RunMode.Async)]
+        [RequireContext(ContextType.DM)]
+        public async Task ReportDm([Remainder] string text)
+        {
+            var userdata = DatabaseService.UserData(Context.User).FirstOrDefault();
+            if (userdata.Level <= 10) await ReplyAsync("You need to be level 10 or above to use the report system.");
+
+            await ReplyAsync("Report sent.");
+        }
     }
 }
