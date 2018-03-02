448a85f0de59feaafdf555a313c963af57f9a3db
Sphexator <kristoffer.tunlid@gmail.com>
2018-03-02 19:54:33 +0100

    initial commit

---
+++ Jibril/Modules/Report/Report.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Addons.Interactive;
+
+namespace Jibril.Modules.Report
+{
+    public class Report : InteractiveBase
+    {
+
+    }
+}
