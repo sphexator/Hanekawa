8a701e64fa767f439acad57ce5c36621773a2ba7
Sphexator <kristoffer.tunlid@gmail.com>
2018-04-27 07:38:25 +0200

    string extension fix

--- Jibril/Extensions/StringExtension.cs
+++ Jibril/Extensions/StringExtension.cs
@@ -10,7 +10,7 @@ namespace Jibril.Extensions
             StringBuilder sb = new StringBuilder();
             foreach (char c in str)
             {
-                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_')
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_' || c == ' ')
                 {
                     sb.Append(c);
                 }
