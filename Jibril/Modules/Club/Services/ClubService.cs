0a0625f356dff2db91f7750df7164a1572462f39
Sphexator <kristoffer.tunlid@gmail.com>
2018-04-28 15:14:39 +0200

    Sorting using :awau:

--- Jibril/Modules/Club/Services/ClubService.cs
+++ Jibril/Modules/Club/Services/ClubService.cs
@@ -1,13 +1,18 @@
 ﻿using Discord;
+using Discord.WebSocket;
 using Jibril.Services;
 using System.Linq;
-using Discord.WebSocket;
 
 namespace Jibril.Modules.Fleet.Services
 {
     public class ClubService
     {
         private readonly DiscordSocketClient _client;
+
+        private static readonly OverwritePermissions DenyOverwrite = new OverwritePermissions(readMessages: PermValue.Deny);
+        private static readonly OverwritePermissions AllowOverwrite = new OverwritePermissions(readMessages: PermValue.Allow, attachFiles: PermValue.Allow, embedLinks: PermValue.Allow);
+        private static readonly OverwritePermissions LeaderOverwrite = new OverwritePermissions(manageMessages: PermValue.Allow);
+
         public ClubService(DiscordSocketClient client)
         {
             _client = client;
@@ -31,11 +36,24 @@ namespace Jibril.Modules.Fleet.Services
 
         public void Promote(IUser user)
         {
+            var clubData = ClubDb.UserClubData(user).FirstOrDefault();
+            if (clubData == null) return;
+            if (clubData.Rank <= 2) return;
             ClubDb.Promote(user);
         }
         public void Demote(IUser user)
         {
+            var clubData = ClubDb.UserClubData(user).FirstOrDefault();
+            if (clubData == null) return;
+            if (clubData.Rank >= 3) return;
             ClubDb.Demote(user);
+        }
+        public void PromoteLeader(IUser user)
+        {
+            var club = ClubDb.UserClubData(user).FirstOrDefault();
+            if (club.Rank != 2 || club == null) return;
+            var clubId = ClubDb.GetClubs().First(x => x.Id == club.ClubId);
+            ClubDb.PromoteLeader(user, clubId.Id);
         }
     }
 }
