17dca0939235ccbfbd20538456ef974360821648
Sphexator <kristoffer.tunlid@gmail.com>
2018-09-15 13:28:49 +0200

    minor fix

--- Jibril/Modules/Administration/Event.cs
+++ Jibril/Modules/Administration/Event.cs
@@ -11,58 +11,87 @@ using Microsoft.EntityFrameworkCore;
 namespace Hanekawa.Modules.Administration
 {
     [Summary("Event modules for managing events, add participants and award them all at once")]
+    [RequireContext(ContextType.Guild)]
     public class Event : InteractiveBase
     {
         [Command("Event add")]
         [Summary("Add event participants to the event payout queue (handled by server admins)")]
-        public async Task AddEventParticipantsAsync(IGuildUser user, int amount = 100){
-            using(var db = new DbService()){
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        public async Task AddEventParticipantsAsync(IGuildUser user, int amount = 100)
+        {
+            using (var db = new DbService())
+            {
                 var userdata = await db.GetOrCreateEventParticipant(Context.User as SocketGuildUser);
                 userdata.Amount = userdata.Amount + amount;
                 await db.SaveChangesAsync();
-                await ReplyAsync(null, false, new EmbedBuilder().Reply($"Added {user.Mention} to the payout queue with an addition of {amount} special credit", Color.Green.RawValue).Build());
+                await ReplyAsync(null, false,
+                    new EmbedBuilder()
+                        .Reply($"Added {user.Mention} to the payout queue with an addition of {amount} special credit",
+                            Color.Green.RawValue).Build());
             }
         }
 
         [Command("Event Remove")]
         [Summary("Remove users from the event payout queue")]
-        public async Task RemoveEventParticipantAsync(IGuildUser user){
-            using(var db = new DbService()){
-                var userdata = await db.EventPayouts.FirstOrDefaultAsync(x => x.GuildId == Context.Guild.Id && x.UserId == user.Id);
-                if(userdata == null){
-                    await ReplyAsync(null, false, new EmbedBuilder().Reply($"Couldn't find {user.Mention} in the event payout queue!", Color.Red.RawValue).Build());
+        [RequireUserPermission(GuildPermission.ManageGuild)]
+        public async Task RemoveEventParticipantAsync(IGuildUser user)
+        {
+            using (var db = new DbService())
+            {
+                var userdata =
+                    await db.EventPayouts.FirstOrDefaultAsync(x =>
+                        x.GuildId == Context.Guild.Id && x.UserId == user.Id);
+                if (userdata == null)
+                {
+                    await ReplyAsync(null, false,
+                        new EmbedBuilder().Reply($"Couldn't find {user.Mention} in the event payout queue!",
+                            Color.Red.RawValue).Build());
                     return;
                 }
+
                 db.EventPayouts.Remove(userdata);
                 await db.SaveChangesAsync();
-                await ReplyAsync(null, false, new EmbedBuilder().Reply($"Removed {user.Mention} with an amount of {userdata.Amount} from the event payout queue.", Color.Green.RawValue).Build());
+                await ReplyAsync(null, false,
+                    new EmbedBuilder()
+                        .Reply(
+                            $"Removed {user.Mention} with an amount of {userdata.Amount} from the event payout queue.",
+                            Color.Green.RawValue).Build());
             }
         }
 
         [Command("Event Adjust")]
         [Summary("Adjust the reward for a user in the event payout queue")]
-        public async Task AdjustEventParticipant(IGuildUser user, int amount){
-            using(var db = new DbService()){
+        [RequireUserPermission(GuildPermission.ManageGuild)]
+        public async Task AdjustEventParticipant(IGuildUser user, int amount)
+        {
+            using (var db = new DbService())
+            {
                 var userdata = await db.GetOrCreateEventParticipant(user as SocketGuildUser);
                 userdata.Amount = amount;
                 await db.SaveChangesAsync();
-                await ReplyAsync(null, false, new EmbedBuilder().Reply($"Adjusted {user.Mention} special credit to {amount}", Color.Green.RawValue).Build());
+                await ReplyAsync(null, false,
+                    new EmbedBuilder()
+                        .Reply($"Adjusted {user.Mention} special credit to {amount}", Color.Green.RawValue).Build());
             }
         }
 
         [Command("event list")]
         [Summary("List all users in the event payout queue")]
-        public async Task ListEventParticipantsAsync(){
-            using(var db = new DbService()){
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        public async Task ListEventParticipantsAsync()
+        {
+            using (var db = new DbService())
+            {
                 var users = await db.EventPayouts.Where(x => x.GuildId == Context.Guild.Id).ToListAsync();
-                if(users.Count <= 0){
-                    await ReplyAsync(null, false, new EmbedBuilder().Reply($"No users in the queue.", Color.Red.RawValue).Build());
+                if (users.Count <= 0)
+                {
+                    await ReplyAsync(null, false,
+                        new EmbedBuilder().Reply($"No users in the queue.", Color.Red.RawValue).Build());
                     return;
                 }
+
                 string content = null;
-                foreach(var x in users){
-                    content += $"{Context.Guild.GetUser(x.UserId).Mention} - {x.Amount}\n";
-                }
+                foreach (var x in users) content += $"{Context.Guild.GetUser(x.UserId).Mention} - {x.Amount}\n";
                 await ReplyAsync(null, false, new EmbedBuilder().Reply(content).Build());
             }
         }
