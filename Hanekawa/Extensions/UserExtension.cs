f1067b703f2eb8e86d687b52207fe119b2f06788
Sphexator <kristoffer.tunlid@gmail.com>
2019-01-02 20:10:21 +0100

    minor change

--- Hanekawa/Extensions/UserExtension.cs
+++ Hanekawa/Extensions/UserExtension.cs
@@ -5,29 +5,16 @@ namespace Hanekawa.Extensions
 {
     public static class UserExtension
     {
-        public static string GetName(this IGuildUser user)
-        {
-            return user.Nickname ?? user.Username;
-        }
+        public static string GetName(this IGuildUser user) => user.Nickname ?? user.Username;
 
-        public static string GetAvatar(this IUser user)
-        {
-            return user.GetAvatarUrl(ImageFormat.Auto, 1024) ?? user.GetDefaultAvatarUrl();
-        }
+        public static string GetAvatar(this IUser user) =>
+            user.GetAvatarUrl(ImageFormat.Auto, 1024) ?? user.GetDefaultAvatarUrl();
 
-        public static string GetGame(this IUser user)
-        {
-            return user.Activity.Name ?? "N/A";
-        }
+        public static string GetGame(this IUser user) => user.Activity.Name ?? "N/A";
 
-        public static bool HierarchyCheck(this SocketGuildUser user, SocketGuildUser userNumberTwo)
-        {
-            return user.Hierarchy > userNumberTwo.Hierarchy;
-        }
+        public static bool HierarchyCheck(this SocketGuildUser user, SocketGuildUser userNumberTwo) =>
+            user.Hierarchy < userNumberTwo.Hierarchy;
 
-        public static bool HierarchyCheck(this SocketGuildUser user, IRole role)
-        {
-            return user.Hierarchy > role.Position;
-        }
+        public static bool HierarchyCheck(this SocketGuildUser user, IRole role) => user.Hierarchy < role.Position;
     }
 }
