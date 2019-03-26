b503a3d8a08b6e7118af9f6c35cd73c646ecd03b
Sphexator <kristoffer.tunlid@gmail.com>
2019-03-26 15:23:07 +0100

    Cleanup banners on leave

--- Hanekawa/Bot/Services/Welcome/WelcomeService.cs
+++ Hanekawa/Bot/Services/Welcome/WelcomeService.cs
@@ -1,14 +1,11 @@
-using System;
-using System.Collections.Concurrent;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
 using Hanekawa.Addons.Database;
 using Hanekawa.Addons.Database.Extensions;
-using Hanekawa.Addons.Database.Tables.Config.Guild;
 using Hanekawa.Bot.Services.ImageGen;
-using Microsoft.Extensions.Caching.Memory;
 
 namespace Hanekawa.Bot.Services.Welcome
 {
@@ -18,11 +15,6 @@ namespace Hanekawa.Bot.Services.Welcome
         private readonly WelcomeImg _img;
         private readonly DbService _db;
         
-        private readonly ConcurrentDictionary<ulong, MemoryCache> _cooldown 
-            = new ConcurrentDictionary<ulong, MemoryCache>();
-        private readonly ConcurrentDictionary<ulong, MemoryCache> _ratelimit 
-            = new ConcurrentDictionary<ulong, MemoryCache>();
-        
         public WelcomeService(DiscordSocketClient client, DbService db, WelcomeImg img)
         {
             _client = client;
@@ -30,6 +22,7 @@ namespace Hanekawa.Bot.Services.Welcome
             _img = img;
 
             _client.UserJoined += WelcomeUser;
+            _client.LeftGuild += LeftGuild;
         }
 
         private Task WelcomeUser(SocketGuildUser user)
@@ -63,20 +56,15 @@ namespace Hanekawa.Bot.Services.Welcome
             return Task.CompletedTask;
         }
 
-        private bool OnCooldown(SocketGuildUser user)
+        private Task LeftGuild(SocketGuild guild)
         {
-            var users = _cooldown.GetOrAdd(user.Guild.Id, new MemoryCache(new MemoryCacheOptions()));
-            if (users.TryGetValue(user.Id, out _)) return true;
-            users.Set(user.Id, user, TimeSpan.FromSeconds(60));
-            return false;
-        }
-        
-        private bool IsRatelimited(SocketGuildUser user, WelcomeConfig cfg)
-        {
-            var users = _ratelimit.GetOrAdd(user.Guild.Id, new MemoryCache(new MemoryCacheOptions()));
-            if (users.Count + 1 >= cfg.Limit) return true;
-            users.Set(user.Id, user.Id, TimeSpan.FromSeconds(5));
-            return false;
+            _ = Task.Run(async () =>
+            {
+                var banners = _db.WelcomeBanners.Where(x => x.GuildId == guild.Id);
+                _db.WelcomeBanners.RemoveRange(banners);
+                await _db.SaveChangesAsync();
+            });
+            return Task.CompletedTask;
         }
     }
 }
