e2554485387681f3035704bf53e50de87ca17125
Sphexator <Kristoffer.tunlid@gmail.com>
2019-03-28 17:31:13 +0100

    stuff with drop service

--- Hanekawa/Bot/Services/Drop/DropData.cs
+++ Hanekawa/Bot/Services/Drop/DropData.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Concurrent;
+using Discord.WebSocket;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace Hanekawa.Bot.Services.Drop
@@ -19,26 +21,46 @@ namespace Hanekawa.Bot.Services.Drop
         private readonly ConcurrentDictionary<ulong, MemoryCache> _userCooldown =
             new ConcurrentDictionary<ulong, MemoryCache>();
 
-        private bool IsDropMessage()
+        
+        private bool IsDropChannel(SocketTextChannel channel)
         {
-            
-        }
-
-        private bool IsDropChannel()
-        {
-            
-        }
-
-        private bool OnUserCooldown()
-        {
-            
-        }
-
-        private bool OnGuildCooldown()
-        {
-            
+            var channels = _lootChannels.GetOrAdd(channel.Guild.Id, new ConcurrentDictionary<ulong, bool>());
+            return channels.TryGetValue(channel.Id, out _);
         }
         
+        private bool IsDropMessage(ulong guildId, ulong messageId, out bool special)
+        {
+            var regular = _normalLoot.GetOrAdd(guildId, new MemoryCache(new MemoryCacheOptions()));
+            var spawned = _spawnedLoot.GetOrAdd(guildId, new MemoryCache(new MemoryCacheOptions()));
+            if (regular.TryGetValue(messageId, out _))
+            {
+                special = false;
+                return true;
+            }
+
+            if (spawned.TryGetValue(messageId, out _))
+            {
+                special = true;
+                return true;
+            }
+
+            special = false;
+            return false;
+        }
         
+        private bool OnUserCooldown(SocketGuildUser user)
+        {
+            var users = _userCooldown.GetOrAdd(user.Guild.Id, new MemoryCache(new MemoryCacheOptions()));
+            if (users.TryGetValue(user.Id, out _)) return true;
+            users.Set(user.Id, TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1));
+            return false;
+        }
+
+        private bool OnGuildCooldown(SocketGuild guild)
+        {
+            if (_guildCooldown.TryGetValue(guild.Id, out _)) return true;
+            _guildCooldown.Set(guild.Id, true, TimeSpan.FromMinutes(1));
+            return false;
+        }
     }
 }
