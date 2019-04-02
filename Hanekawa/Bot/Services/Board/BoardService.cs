977c68486216c6f7c5b75908b38cadcc6831a019
Sphexator <Kristoffer.tunlid@gmail.com>
2019-04-02 22:33:10 +0200

    Kuuthinking

--- Hanekawa/Bot/Services/Board/BoardService.cs
+++ Hanekawa/Bot/Services/Board/BoardService.cs
@@ -1,8 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Discord;
+using Discord.Rest;
 using Discord.WebSocket;
 using Hanekawa.Addons.Database;
+using Hanekawa.Addons.Database.Extensions;
+using Hanekawa.Addons.Database.Tables.Config.Guild;
 using Hanekawa.Entities.Interfaces;
+using Hanekawa.Extensions;
 
 namespace Hanekawa.Bot.Services.Board
 {
@@ -25,8 +31,8 @@ namespace Hanekawa.Bot.Services.Board
             _client.ReactionRemoved += ReactionRemovedAsync;
             _client.ReactionsCleared += ReactionsClearedAsync;
         }
-
-        private Task ReactionAddedAsync(Cacheable<IUserMessage, ulong> message, ISocketMessageChannel channel, SocketReaction rct)
+        
+        private Task ReactionAddedAsync(Cacheable<IUserMessage, ulong> msg, ISocketMessageChannel channel, SocketReaction rct)
         {
             _ = Task.Run(async () =>
             {
@@ -36,11 +42,29 @@ namespace Hanekawa.Bot.Services.Board
                 if (user.IsBot) return;
                 var emote = await GetEmote(user.Guild);
                 if (!rct.Emote.Equals(emote)) return;
+                var cfg = await _db.GetOrCreateBoardConfigAsync(ch.Guild);
+                if (!cfg.Channel.HasValue) return;
+                
+                var message = await msg.GetOrDownloadAsync();
+                var stat = await _db.GetOrCreateBoard(ch.Guild, message);
+                var giver = await _db.GetOrCreateUserData(user);
+                var receiver = await _db.GetOrCreateUserData(message.Author as SocketGuildUser);
+                receiver.StarReceived++;
+                giver.StarGiven++;
+                stat.StarAmount++;
+                await _db.SaveChangesAsync();
+                IncreaseReactionAmount(user.Guild, message);
+                if (GetReactionAmount(user.Guild, message) >= 4 && !stat.Boarded.HasValue)
+                {
+                    stat.Boarded = new DateTimeOffset(DateTime.UtcNow);
+                    await _db.SaveChangesAsync();
+                    await SendMessageAsync(user, message, cfg);
+                }
             });
             return Task.CompletedTask;
         }
 
-        private Task ReactionRemovedAsync(Cacheable<IUserMessage, ulong> message, ISocketMessageChannel channel, SocketReaction rct)
+        private Task ReactionRemovedAsync(Cacheable<IUserMessage, ulong> msg, ISocketMessageChannel channel, SocketReaction rct)
         {
             _ = Task.Run(async () =>
             {
@@ -50,11 +74,20 @@ namespace Hanekawa.Bot.Services.Board
                 if (user.IsBot) return;
                 var emote = await GetEmote(user.Guild);
                 if (!rct.Emote.Equals(emote)) return;
-
+                
+                var message = await msg.GetOrDownloadAsync(); 
+                var stat = await _db.GetOrCreateBoard(ch.Guild, message);
+                var giver = await _db.GetOrCreateUserData(user);
+                var receiver = await _db.GetOrCreateUserData(message.Author as SocketGuildUser);
+                receiver.StarReceived--;
+                giver.StarGiven--;
+                stat.StarAmount--;
+                await _db.SaveChangesAsync();
+                DecreaseReactionAmount(user.Guild, message);
             });
             return Task.CompletedTask;
         }
-
+        
         private Task ReactionsClearedAsync(Cacheable<IUserMessage, ulong> message, ISocketMessageChannel channel)
         {
             _ = Task.Run(async () =>
@@ -69,10 +102,26 @@ namespace Hanekawa.Bot.Services.Board
             });
             return Task.CompletedTask;
         }
-
-        private async Task SendMessageAsync()
+        
+        private async Task<RestUserMessage> SendMessageAsync(SocketGuildUser user, IUserMessage msg, BoardConfig cfg)
         {
-
+            var embed = new EmbedBuilder
+            {
+                Author = new EmbedAuthorBuilder
+                {
+                    Name = $"{user.GetName()} (Jump!)",
+                    Url = user.GetAvatar(),
+                    IconUrl = msg.GetJumpUrl()
+                },
+                Color = user.Roles.OrderByDescending(x => x.Position)
+                    .FirstOrDefault(x => x.Color.RawValue != 0)?.Color,
+                Description = msg.Content,
+                Footer = new EmbedFooterBuilder { Text = msg.Channel.Name },
+                Timestamp = msg.Timestamp,
+            };
+            if (msg.Attachments.Count > 0) embed.ImageUrl = msg.Attachments.First().Url;
+            var channel = user.Guild.GetTextChannel(cfg.Channel.Value);
+            return await channel.SendMessageAsync(null, false, embed.Build());
         }
     }
 }
