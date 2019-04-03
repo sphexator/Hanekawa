d4b1da2ddb32119e44155439a2cabb131f152085
Sphexator <kristoffer.tunlid@gmail.com>
2019-04-03 17:27:09 +0200

    Added voice achievements

--- Hanekawa/Bot/Services/Achievement/VoiceAchievement.cs
+++ Hanekawa/Bot/Services/Achievement/VoiceAchievement.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Hanekawa.Addons.Database.Extensions;
 using Hanekawa.Addons.Database.Tables.Achievement;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,7 +12,56 @@ namespace Hanekawa.Bot.Services.Achievement
     {
         public async Task TotalTime(SocketGuildUser user, TimeSpan time)
         {
+            var achievements = await _db.Achievements.Where(x => x.TypeId == Voice && x.AchievementNameId == 15).ToListAsync();
+            var progress = await _db.GetOrCreateAchievementProgress(user, Voice);
+            if (achievements == null || achievements.Count == 0) return;
+            if (progress == null) return;
 
+            var totalTime = Convert.ToInt32(time.TotalMinutes);
+            var progCount = progress.Count + totalTime;
+            if (achievements.Any(x => x.Requirement == progCount && !x.Once))
+            {
+                var achieve = achievements.FirstOrDefault(x =>
+                    x.Requirement == progCount && x.Once == false);
+                if (achieve != null)
+                {
+                    var data = new AchievementUnlock
+                    {
+                        AchievementId = achieve.AchievementId,
+                        TypeId = Voice,
+                        UserId = user.Id,
+                        Achievement = achieve
+                    };
+                    await _db.AchievementUnlocks.AddAsync(data);
+                    await _db.SaveChangesAsync();
+                }
+            }
+            else
+            {
+                var below = achievements.Where(x => x.Requirement < progCount && !x.Once).ToList();
+                if (below.Count != 0)
+                {
+                    var unlocked = await _db.AchievementUnlocks.Where(x => x.TypeId == Voice).ToListAsync();
+                    foreach (var x in below)
+                    {
+                        if (unlocked.Any(y => y.AchievementId == x.AchievementId)) continue;
+
+                        var data = new AchievementUnlock
+                        {
+                            AchievementId = x.AchievementId,
+                            TypeId = Voice,
+                            UserId = user.Id,
+                            Achievement = x
+                        };
+                        await _db.AchievementUnlocks.AddAsync(data);
+                    }
+
+                    await _db.SaveChangesAsync();
+                }
+            }
+
+            progress.Count = progCount;
+            await _db.SaveChangesAsync();
         }
 
         public async Task TimeAtOnce(SocketGuildUser user, TimeSpan time)
