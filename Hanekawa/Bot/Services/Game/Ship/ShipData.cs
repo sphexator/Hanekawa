1c2f95bb4a843168be3d8895fa6d2ac844cf9384
Sphexator <kristoffer.tunlid@gmail.com>
2019-04-04 01:33:25 +0200

    game service data added

---
+++ Hanekawa/Bot/Services/Game/Ship/ShipData.cs
@@ -0,0 +1,26 @@
+﻿using Hanekawa.Addons.Database.Tables.BotGame;
+using System.Collections.Concurrent;
+
+namespace Hanekawa.Bot.Services.Game.Ship
+{
+    public partial class ShipGameService
+    {
+        private readonly ConcurrentDictionary<int, GameEnemy> _regularEnemies
+            = new ConcurrentDictionary<int, GameEnemy>();
+
+        private readonly ConcurrentDictionary<int, GameEnemy> _rareEnemies
+            = new ConcurrentDictionary<int, GameEnemy>();
+
+        private readonly ConcurrentDictionary<int, GameEnemy> _eliteEnemies
+            = new ConcurrentDictionary<int, GameEnemy>();
+
+        private readonly ConcurrentDictionary<ulong, ConcurrentDictionary<ulong, GameEnemy>> _existingBattles
+            = new ConcurrentDictionary<ulong, ConcurrentDictionary<ulong, GameEnemy>>();
+
+        private readonly ConcurrentDictionary<ulong, ConcurrentDictionary<ulong, bool>> _activeBattles
+            = new ConcurrentDictionary<ulong, ConcurrentDictionary<ulong, bool>>();
+
+        private readonly ConcurrentDictionary<ulong, ConcurrentDictionary<ulong, bool>> _activeDuels
+            = new ConcurrentDictionary<ulong, ConcurrentDictionary<ulong, bool>>();
+    }
+}
---
+++ Hanekawa/Bot/Services/Game/Ship/ShipGameService.cs
@@ -0,0 +1,48 @@
+﻿using Hanekawa.Addons.Database;
+using Hanekawa.Bot.Services.ImageGen;
+using Hanekawa.Entities.Interfaces;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Hanekawa.Bot.Services.Game.Ship
+{
+    public partial class ShipGameService : INService
+    {
+        private readonly DbService _db;
+        private readonly HttpClient _client;
+        private readonly Random _random;
+        private readonly ImageGenerator _img;
+
+        public ShipGameService(DbService db, HttpClient client, Random random, ImageGenerator img)
+        {
+            _db = db;
+            _client = client;
+            _random = random;
+            _img = img;
+
+            foreach (var x in _db.GameEnemies)
+            {
+                if (x.Elite) _eliteEnemies.TryAdd(x.Id, x);
+                else if (x.Rare) _rareEnemies.TryAdd(x.Id, x);
+                else _regularEnemies.TryAdd(x.Id, x);
+            }
+        }
+
+        public async Task SearchAsync()
+        {
+
+        }
+
+        public async Task PvPBattle()
+        {
+
+        }
+
+        public async Task PvEBattle()
+        {
+
+        }
+    }
+}
