72c15d5914cd2ec02dcba7d024dfb9947ef550d8
Sphexator <kristoffer.tunlid@gmail.com>
2019-04-03 17:13:19 +0200

    Added level achievements

--- Hanekawa/Bot/Services/Achievement/LevelAchievement.cs
+++ Hanekawa/Bot/Services/Achievement/LevelAchievement.cs
@@ -1,19 +1,100 @@
-﻿using Discord.WebSocket;
+﻿using System.Linq;
+using Discord.WebSocket;
 using Hanekawa.Addons.Database.Tables.Account;
 using System.Threading.Tasks;
+using Hanekawa.Addons.Database.Tables.Achievement;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hanekawa.Bot.Services.Achievement
 {
     public partial class AchievementService
     {
-        public async Task ServerLevel(SocketGuildUser user, Account userdata)
+        public async Task ServerLevel(SocketGuildUser user, Account userData)
         {
+            var achievements = await _db.Achievements.Where(x => x.TypeId == Level && !x.Once && !x.Global).ToListAsync();
+            if (achievements == null || achievements.Count == 0) return;
+            var unlocked = await _db.AchievementUnlocks.Where(x => x.UserId == user.Id).ToListAsync();
+            if (achievements.Any(x => x.Requirement == userData.Level))
+            {
+                var achieve = achievements.First(x => x.Requirement == userData.Level);
+                var check = unlocked.FirstOrDefault(x => x.Achievement == achieve);
+                if (check != null) return;
+                var data = new AchievementUnlock
+                {
+                    AchievementId = achieve.AchievementId,
+                    TypeId = Level,
+                    UserId = user.Id,
+                    Achievement = achieve
+                };
+                await _db.AchievementUnlocks.AddAsync(data);
+                await _db.SaveChangesAsync();
+            }
+            else
+            {
+                var belowAchieves = achievements
+                    .Where(x => x.Requirement < userData.Level).ToList();
+                if (belowAchieves.Count > 0)
+                {
+                    foreach (var x in belowAchieves)
+                    {
+                        if (unlocked.Any(y => y.AchievementId == x.AchievementId)) continue;
 
+                        var data = new AchievementUnlock
+                        {
+                            AchievementId = x.AchievementId,
+                            TypeId = Level,
+                            UserId = user.Id,
+                            Achievement = x
+                        };
+                        await _db.AchievementUnlocks.AddAsync(data);
+                    }
+                    await _db.SaveChangesAsync();
+                }
+            }
         }
 
-        public async Task GlobalLevel(SocketGuildUser user, AccountGlobal userdata)
+        public async Task GlobalLevel(SocketGuildUser user, AccountGlobal userData)
         {
+            var achievements = await _db.Achievements
+                .Where(x => x.TypeId == Level && !x.Once && x.Global).ToListAsync();
+            if (achievements == null || achievements.Count == 0) return;
 
+            if (achievements.Any(x => x.Requirement == userData.Level))
+            {
+                var achieve = achievements.First(x => x.Requirement == userData.Level);
+                var data = new AchievementUnlock
+                {
+                    AchievementId = achieve.AchievementId,
+                    TypeId = Level,
+                    UserId = user.Id,
+                    Achievement = achieve
+                };
+                await _db.AchievementUnlocks.AddAsync(data);
+                await _db.SaveChangesAsync();
+            }
+            else
+            {
+                var belowAchieves = achievements.Where(x => x.Requirement < userData.Level).ToList();
+                if (belowAchieves.Count > 0)
+                {
+                    var unlocked = await _db.AchievementUnlocks.Where(x => x.UserId == user.Id).ToListAsync();
+                    foreach (var x in belowAchieves)
+                    {
+                        if (unlocked.Any(y => y.AchievementId == x.AchievementId)) continue;
+
+                        var data = new AchievementUnlock
+                        {
+                            AchievementId = x.AchievementId,
+                            TypeId = Level,
+                            UserId = user.Id,
+                            Achievement = x
+                        };
+                        await _db.AchievementUnlocks.AddAsync(data);
+                    }
+
+                    await _db.SaveChangesAsync();
+                }
+            }
         }
     }
 }
--- Hanekawa/Bot/Services/Achievement/VoiceAchievement.cs
+++ Hanekawa/Bot/Services/Achievement/VoiceAchievement.cs
@@ -1,6 +1,9 @@
 ﻿using Discord.WebSocket;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Hanekawa.Addons.Database.Tables.Achievement;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hanekawa.Bot.Services.Achievement
 {
@@ -13,7 +16,48 @@ namespace Hanekawa.Bot.Services.Achievement
 
         public async Task TimeAtOnce(SocketGuildUser user, TimeSpan time)
         {
+            var achievements = await _db.Achievements.Where(x => x.TypeId == Voice).ToListAsync();
+            if (achievements == null || achievements.Count == 0) return;
 
+            var totalTime = Convert.ToInt32(time.TotalMinutes);
+            if (achievements.Any(x => x.Requirement == totalTime && x.AchievementNameId == 8))
+            {
+                var achieve = achievements.First(x => x.Requirement == totalTime && x.Once);
+                var unlockCheck = await _db.AchievementUnlocks.FirstOrDefaultAsync(x =>
+                    x.AchievementId == achieve.AchievementId && x.UserId == user.Id);
+                if (unlockCheck != null) return;
+
+                var data = new AchievementUnlock
+                {
+                    AchievementId = achieve.AchievementId,
+                    TypeId = Voice,
+                    UserId = user.Id,
+                    Achievement = achieve
+                };
+                await _db.AchievementUnlocks.AddAsync(data);
+                await _db.SaveChangesAsync();
+            }
+            else
+            {
+                var below = achievements.Where(x => x.Requirement < totalTime).ToList();
+                var unlock = await _db.AchievementUnlocks.Where(x => x.UserId == user.Id && x.TypeId == Voice)
+                    .ToListAsync();
+                foreach (var x in below)
+                {
+                    if (unlock.Any(y => y.AchievementId == x.AchievementId)) continue;
+
+                    var data = new AchievementUnlock
+                    {
+                        AchievementId = x.AchievementId,
+                        TypeId = Level,
+                        UserId = user.Id,
+                        Achievement = x
+                    };
+                    await _db.AchievementUnlocks.AddAsync(data);
+                }
+
+                await _db.SaveChangesAsync();
+            }
         }
     }
 }
