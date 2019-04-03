79ac0d04de57c52e0d9461861bcbb1b61d019ec5
Sphexator <kristoffer.tunlid@gmail.com>
2019-04-03 16:22:54 +0200

    Added drop achievement

--- Hanekawa/Bot/Services/Achievement/DropAchievement.cs
+++ Hanekawa/Bot/Services/Achievement/DropAchievement.cs
@@ -2,6 +2,7 @@
 using Discord.WebSocket;
 using System.Threading.Tasks;
 using Hanekawa.Addons.Database.Extensions;
+using Hanekawa.Addons.Database.Tables.Achievement;
 using Microsoft.EntityFrameworkCore;
 
 namespace Hanekawa.Bot.Services.Achievement
@@ -11,7 +12,50 @@ namespace Hanekawa.Bot.Services.Achievement
         public async Task DropClaim(SocketGuildUser user)
         {
             var achievements = await _db.Achievements.Where(x => x.TypeId == Drop).ToListAsync();
-            var progress = await _db.GetAchievementProgress(user, Drop);
+            var progress = await _db.GetOrCreateAchievementProgress(user, Drop);
+
+            if (progress == null) return;
+            if (achievements == null) return;
+            var progCount = progress.Count + 1;
+            if (achievements.Any(x => x.Requirement == progCount && !x.Once))
+            {
+                var achieve = achievements.FirstOrDefault(x => x.Requirement == progCount && !x.Once);
+                if (achieve == null) return;
+                var data = new AchievementUnlock
+                {
+                    AchievementId = achieve.AchievementId,
+                    TypeId = Drop,
+                    Achievement = achieve,
+                    UserId = user.Id
+                };
+                await _db.AchievementUnlocks.AddAsync(data);
+                await _db.SaveChangesAsync();
+            }
+            else
+            {
+                var below = achievements.Where(x => x.Requirement < progCount && !x.Once).ToList();
+                if (below.Count != 0)
+                {
+                    var unlocked = await _db.AchievementUnlocks.Where(x => x.UserId == user.Id).ToListAsync();
+                    foreach (var x in below)
+                    {
+                        if (unlocked.Any(y => y.AchievementId == x.AchievementId)) continue;
+                        var data = new AchievementUnlock
+                        {
+                            AchievementId = x.AchievementId,
+                            TypeId = Drop,
+                            UserId = user.Id,
+                            Achievement = x
+                        };
+                        await _db.AchievementUnlocks.AddAsync(data);
+                    }
+
+                    await _db.SaveChangesAsync();
+                }
+            }
+
+            progress.Count++;
+            await _db.SaveChangesAsync();
         }
     }
 }
