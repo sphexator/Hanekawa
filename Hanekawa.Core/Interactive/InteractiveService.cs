82f7f92e19fa65aabd0df284dd5dccfb83601a04
Sphexator <kristoffer.tunlid@gmail.com>
2019-05-14 19:00:39 +0200

    small change in interactive service (syntax)

--- Hanekawa.Core/Interactive/InteractiveService.cs
+++ Hanekawa.Core/Interactive/InteractiveService.cs
@@ -18,14 +18,10 @@ namespace Hanekawa.Core.Interactive
 
         // helpers to allow DI containers to resolve without a custom factory
         public InteractiveService(DiscordSocketClient discord, InteractiveServiceConfig config = null)
-        {
-            new InteractiveService((BaseSocketClient) discord, config);
-        }
+            : this((BaseSocketClient)discord, config) { }
 
         public InteractiveService(DiscordShardedClient discord, InteractiveServiceConfig config = null)
-        {
-            new InteractiveService((BaseSocketClient) discord, config);
-        }
+            : this((BaseSocketClient)discord, config) { }
 
         public InteractiveService(BaseSocketClient discord, InteractiveServiceConfig config = null)
         {
