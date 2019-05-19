554af8ffc39d59b4d16c2106ccd0596ebba2cef8
Sphexator <kristoffer.tunlid@gmail.com>
2019-05-19 19:59:30 +0200

    Added exp ignore commands

--- Hanekawa/Bot/Modules/Level/Experience.cs
+++ Hanekawa/Bot/Modules/Level/Experience.cs
@@ -8,6 +8,7 @@ using Hanekawa.Database;
 using Hanekawa.Database.Extensions;
 using Hanekawa.Database.Tables.Config;
 using Hanekawa.Extensions.Embed;
+using Microsoft.EntityFrameworkCore;
 using Qmmands;
 
 namespace Hanekawa.Bot.Modules.Level
@@ -117,7 +118,7 @@ namespace Hanekawa.Bot.Modules.Level
         {
             using (var db = new DbService())
             {
-                var channeList = _exp.ServerChannelReduction.GetOrAdd(Context.Guild.Id, new HashSet<ulong>());
+                var channeList = _exp.ServerCategoryReduction.GetOrAdd(Context.Guild.Id, new HashSet<ulong>());
                 var content = new StringBuilder();
                 content.AppendLine("Categories added to exp ignore list:");
                 for (var i = 0; i < category.Length; i++)
@@ -154,7 +155,31 @@ namespace Hanekawa.Bot.Modules.Level
         [RequireUserPermission(GuildPermission.ManageGuild)]
         public async Task RemoveExpIgnoreChannel(params SocketTextChannel[] channels)
         {
+            using (var db = new DbService())
+            {
+                var channeList = _exp.ServerChannelReduction.GetOrAdd(Context.Guild.Id, new HashSet<ulong>());
+                var content = new StringBuilder();
+                content.AppendLine("Channels removed from exp ignore list:");
+                for (var i = 0; i < channels.Length; i++)
+                {
+                    var x = channels[i];
+                    if (channeList.TryGetValue(x.Id, out _))
+                    {
+                        channeList.Remove(x.Id);
+                        var data = await db.LevelExpReductions.FirstOrDefaultAsync(z =>
+                            z.GuildId == Context.Guild.Id && z.ChannelId == x.Id);
+                        db.LevelExpReductions.Remove(data);
+                        content.AppendLine($"Removed {x.Mention}");
+                    }
+                    else
+                    {
+                        content.AppendLine($"Can't remove {x.Mention} as it's not added");
+                    }
+                }
 
+                await db.SaveChangesAsync();
+                await Context.ReplyAsync(content.ToString());
+            }
         }
 
         [Name("Remove Exp Ignore Category")]
@@ -164,7 +189,31 @@ namespace Hanekawa.Bot.Modules.Level
         [RequireUserPermission(GuildPermission.ManageGuild)]
         public async Task RemoveExpIgnoreChannel(params SocketCategoryChannel[] category)
         {
+            using (var db = new DbService())
+            {
+                var channeList = _exp.ServerCategoryReduction.GetOrAdd(Context.Guild.Id, new HashSet<ulong>());
+                var content = new StringBuilder();
+                content.AppendLine("Categories removed from exp ignore list:");
+                for (var i = 0; i < category.Length; i++)
+                {
+                    var x = category[i];
+                    if (channeList.TryGetValue(x.Id, out _))
+                    {
+                        channeList.Remove(x.Id);
+                        var data = await db.LevelExpReductions.FirstOrDefaultAsync(z =>
+                            z.GuildId == Context.Guild.Id && z.ChannelId == x.Id);
+                        db.LevelExpReductions.Remove(data);
+                        content.AppendLine($"Removed {x.Name}");
+                    }
+                    else
+                    {
+                        content.AppendLine($"Can't remove {x.Name} as it's not added");
+                    }
+                }
 
+                await db.SaveChangesAsync();
+                await Context.ReplyAsync(content.ToString());
+            }
         }
 
         [Name("Remove Exp Ignore Category")]
@@ -174,7 +223,35 @@ namespace Hanekawa.Bot.Modules.Level
         [RequiredChannel]
         public async Task ExpIgnoreList()
         {
+            var channels = _exp.ServerChannelReduction.GetOrAdd(Context.Guild.Id, new HashSet<ulong>());
+            var categories = _exp.ServerCategoryReduction.GetOrAdd(Context.Guild.Id, new HashSet<ulong>());
+            var result = new List<string>();
+            if (channels.Count == 0 && categories.Count == 0)
+            {
+                result.Add("No channels");
+            }
 
+            if (channels.Count > 0)
+            {
+                foreach (var x in channels)
+                {
+                    var channel = Context.Guild.GetTextChannel(x);
+                    if (channel == null) continue;
+                    result.Add($"Channel: {channel.Mention}");
+                }
+            }
+
+            if (categories.Count > 0)
+            {
+                foreach (var x in categories)
+                {
+                    var category = Context.Guild.GetCategoryChannel(x);
+                    if (category == null) continue;
+                    result.Add($"Category: {category.Name}");
+                }
+            }
+
+            await PagedReplyAsync(result.PaginateBuilder(Context.Guild, "Experience Channel Ignore", null));
         }
     }
 }
