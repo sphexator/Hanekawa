512a6057296b2624c3cc7f8b45d95f97c3ea7d9f
Sphexator <kristoffer.tunlid@gmail.com>
2019-05-22 23:54:00 +0200

    added logging commands

--- Hanekawa/Bot/Modules/Settings/Log.cs
+++ Hanekawa/Bot/Modules/Settings/Log.cs
@@ -1,10 +1,183 @@
-﻿using Hanekawa.Core.Interactive;
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Hanekawa.Bot.Preconditions;
+using Hanekawa.Core.Interactive;
+using Hanekawa.Database;
+using Hanekawa.Database.Extensions;
+using Hanekawa.Extensions.Embed;
+using Qmmands;
 
 namespace Hanekawa.Bot.Modules.Settings
 {
-    public class Log : InteractiveBase 
+    [Name("Log")]
+    [RequireBotPermission(GuildPermission.EmbedLinks)]
+    [RequireUserPermission(GuildPermission.ManageGuild)]
+    public class Log : InteractiveBase
     {
-            // TODO: Log settings, all of them
+        // TODO: Log settings, all of them
+        [Name("Log Join/Leave")]
+        [Command("log join")]
+        [Description("Logs users joining and leaving server, leave empty to disable")]
+        [Remarks("log join #log")]
+        public async Task JoinLogAsync(SocketTextChannel channel = null)
+        {
+            using (var db = new DbService())
+            {
+                var cfg = await db.GetOrCreateLoggingConfigAsync(Context.Guild);
+                if (channel == null)
+                {
+                    cfg.LogJoin = null;
+                    await Context.ReplyAsync("Disabled logging of join/leave!", Color.Green.RawValue);
+                    await db.SaveChangesAsync();
+                    return;
+                }
 
+                cfg.LogJoin = channel.Id;
+                await Context.ReplyAsync($"Set join/leave logging channel to {channel.Mention}!", Color.Green.RawValue);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        [Name("Log Warnings")]
+        [Command("log warn")]
+        [Description("Logs warnings given out from the bot, leave empty to disable")]
+        [Remarks("log warn #log")]
+        public async Task WarnLogAsync(SocketTextChannel channel = null)
+        {
+            using (var db = new DbService())
+            {
+                var cfg = await db.GetOrCreateLoggingConfigAsync(Context.Guild);
+                if (channel == null)
+                {
+                    cfg.LogWarn = null;
+                    await Context.ReplyAsync("Disabled logging of warnings!", Color.Green.RawValue);
+                    await db.SaveChangesAsync();
+                    return;
+                }
+
+                cfg.LogWarn = channel.Id;
+                await Context.ReplyAsync($"Set warn logging channel to {channel.Mention}!", Color.Green.RawValue);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        [Name("Log Messages")]
+        [Command("log messages", "log msgs")]
+        [Description("Logs deleted and updated messages, leave empty to disable")]
+        [Remarks("log msgs #log")]
+        public async Task MessageLogAsync(SocketTextChannel channel = null)
+        {
+            using (var db = new DbService())
+            {
+                var cfg = await db.GetOrCreateLoggingConfigAsync(Context.Guild);
+                if (channel == null)
+                {
+                    cfg.LogMsg = null;
+                    await Context.ReplyAsync("Disabled logging of messages!", Color.Green.RawValue);
+                    await db.SaveChangesAsync();
+                    return;
+                }
+
+                cfg.LogMsg = channel.Id;
+                await Context.ReplyAsync($"Set message logging channel to {channel.Mention}!", Color.Green.RawValue);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        [Name("Log Ban")]
+        [Command("log ban")]
+        [Description("Logs users getting banned and muted, leave empty to disable")]
+        [Remarks("log ban #log")]
+        public async Task BanLogAsync(SocketTextChannel channel = null)
+        {
+            using (var db = new DbService())
+            {
+                var cfg = await db.GetOrCreateLoggingConfigAsync(Context.Guild);
+                if (channel == null)
+                {
+                    cfg.LogBan = null;
+                    await Context.ReplyAsync("Disabled logging of bans!", Color.Green.RawValue);
+                    await db.SaveChangesAsync();
+                    return;
+                }
+
+                cfg.LogBan = channel.Id;
+                await Context.ReplyAsync($"Set ban logging channel to {channel.Mention}!", Color.Green.RawValue);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        [Name("Log User")]
+        [Command("log user")]
+        [Description("Logs user updates, roles/username/nickname, leave empty to disable")]
+        [Remarks("log user #log")]
+        public async Task UserLogAsync(SocketTextChannel channel = null)
+        {
+            using (var db = new DbService())
+            {
+                var cfg = await db.GetOrCreateLoggingConfigAsync(Context.Guild);
+                if (channel == null)
+                {
+                    cfg.LogAvi = null;
+                    await Context.ReplyAsync("Disabled logging of users!", Color.Green.RawValue);
+                    await db.SaveChangesAsync();
+                    return;
+                }
+
+                cfg.LogAvi = channel.Id;
+                await Context.ReplyAsync($"Set user logging channel to {channel.Mention}!", Color.Green.RawValue);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        [Name("Log Auto-Moderator")]
+        [Command("log autmod")]
+        [Description(
+            "Logs activities auto moderator does. Defaults to ban log if this is disabled. Meant if automod entries should be in a different channel.\n Leave empty to disable")]
+        [Remarks("log automod #log")]
+        public async Task AutoModeratorLogAsync(SocketTextChannel channel = null)
+        {
+            using (var db = new DbService())
+            {
+                var cfg = await db.GetOrCreateLoggingConfigAsync(Context.Guild);
+                if (channel == null)
+                {
+                    cfg.LogAutoMod = null;
+                    await Context.ReplyAsync("Disabled separate logging of auto-moderator actions!",
+                        Color.Green.RawValue);
+                    await db.SaveChangesAsync();
+                    return;
+                }
+
+                cfg.LogAutoMod = channel.Id;
+                await Context.ReplyAsync($"Set auto mod log channel to {channel.Mention}!", Color.Green.RawValue);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        [Name("Log Voice")]
+        [Command("log voice")]
+        [Description("Logs voice activities, join/leave/mute/deafen, leave empty to disable")]
+        [Remarks("log voice #log")]
+        public async Task VoiceLogAsync(SocketTextChannel channel = null)
+        {
+            using (var db = new DbService())
+            {
+                var cfg = await db.GetOrCreateLoggingConfigAsync(Context.Guild);
+                if (channel == null)
+                {
+                    cfg.LogVoice = null;
+                    await Context.ReplyAsync("Disabled logging of voice activity!", Color.Green.RawValue);
+                    await db.SaveChangesAsync();
+                    return;
+                }
+
+                cfg.LogVoice = channel.Id;
+                await Context.ReplyAsync($"Set voice activity logging channel to {channel.Mention}!",
+                    Color.Green.RawValue);
+                await db.SaveChangesAsync();
+            }
+        }
     }
 }
--- Hanekawa/Bot/Modules/Settings/Settings.cs
+++ Hanekawa/Bot/Modules/Settings/Settings.cs
@@ -21,6 +21,7 @@ namespace Hanekawa.Bot.Modules.Settings
         [Name("Add prefix")]
         [Command("add prefix", "aprefix")]
         [Description("Adds a prefix to the bot, if it doesn't already exist")]
+        [Remarks("add prefix !")]
         public async Task AddPrefixAsync([Remainder]string prefix)
         {
             using (var db = new DbService())
@@ -36,6 +37,7 @@ namespace Hanekawa.Bot.Modules.Settings
         [Name("Remove prefix")]
         [Command("remove prefix", "rprefix")]
         [Description("Removes a prefix from the bot, if it exists")]
+        [Remarks("remove prefix !")]
         public async Task RemovePrefixAsync([Remainder]string prefix)
         {
             using (var db = new DbService())
@@ -51,6 +53,7 @@ namespace Hanekawa.Bot.Modules.Settings
         [Name("Set embed color")]
         [Command("embed", "set embed")]
         [Description("Changes the embed colour of the bot")]
+        [Remarks("embed 346423")]
         public async Task SetEmbedColorAsync(uint color)
         {
             // TODO: Embed color commands
