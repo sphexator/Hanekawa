949408054e133808d982343101552495280e87ef
Sphexator <kristoffer.tunlid@gmail.com>
2019-06-05 19:10:43 +0200

    Added neko library

---
+++ Hanekawa.NekoLife/NekoClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Hanekawa.NekoLife
+{
+    public class NekoClient
+    {
+        private readonly HttpClient _client;
+        private readonly Random _random;
+        private readonly string _baseUrl = "https://nekos.life/api/v2";
+
+        public NekoClient(HttpClient client, Random random)
+        {
+            _client = client ?? new HttpClient();
+            _random = random ?? new Random();
+        }
+    }
+}
