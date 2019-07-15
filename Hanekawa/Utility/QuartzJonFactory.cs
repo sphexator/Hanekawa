6af791fec872c686033fba0c688873c823b4d0ea
Sphexator <kristoffer.tunlid@gmail.com>
2019-07-15 16:22:41 +0200

    Added scheduling extensions

---
+++ Hanekawa/Utility/QuartzJonFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Hanekawa.Shared.Interfaces;
+using Quartz;
+using Quartz.Spi;
+
+namespace Hanekawa.Utility
+{
+    public class QuartzJonFactory : IJobFactory, INService
+    {
+        private readonly IServiceProvider _services;
+
+        public QuartzJonFactory(IServiceProvider services) => _services = services;
+
+        public IJob NewJob(TriggerFiredBundle bundle, IScheduler scheduler)
+        {
+            var jobDetail = bundle.JobDetail;
+
+            var job = (IJob) _services.GetService(jobDetail.JobType);
+            return job;
+        }
+
+        public void ReturnJob(IJob job)
+        {
+        }
+    }
+}
