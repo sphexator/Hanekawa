d55d03ffa84322592ea5e7f83fbfd4b6d11e7ae5
chris <sphexbot@gmail.com>
2020-08-07 21:54:21 +0200

    Null checking reduced exp checks

--- Hanekawa/Bot/Services/Experience/LevelHandler.cs
+++ Hanekawa/Bot/Services/Experience/LevelHandler.cs
@@ -1,10 +1,11 @@
-using System;
 using System.Collections.Concurrent;
 using System.Threading.Tasks;
 using Discord.WebSocket;
 using Hanekawa.Database;
 using Hanekawa.Database.Tables.Account;
 using Microsoft.Extensions.Logging;
+
+#nullable enable
 
 namespace Hanekawa.Bot.Services.Experience
 {
@@ -16,9 +17,20 @@ namespace Hanekawa.Bot.Services.Experience
         private readonly ConcurrentDictionary<ulong, double> _voiceExpMultiplier =
             new ConcurrentDictionary<ulong, double>();
 
-        public int ExpToNextLevel(Account userdata) => 3 * userdata.Level * userdata.Level + 150;
-        private int ExpToNextLevel(AccountGlobal userdata) => 50 * userdata.Level * userdata.Level + 300;
-        public int ExpToNextLevel(int level) => 3 * level * level + 150;
+        public int ExpToNextLevel(Account userdata)
+        {
+            return 3 * userdata.Level * userdata.Level + 150;
+        }
+
+        private int ExpToNextLevel(AccountGlobal userdata)
+        {
+            return 50 * userdata.Level * userdata.Level + 300;
+        }
+
+        public int ExpToNextLevel(int level)
+        {
+            return 3 * level * level + 150;
+        }
 
         public async Task AddExpAsync(SocketGuildUser user, Account userData, int exp, int credit, DbService db)
         {
@@ -27,23 +39,26 @@ namespace Hanekawa.Bot.Services.Experience
                 userData.Exp = userData.Exp + exp - ExpToNextLevel(userData);
                 userData.Level += 1;
                 await NewLevelManagerAsync(userData, user, db);
-                _log.LogAction(LogLevel.Information, $"(Exp Service | Server) {userData.UserId} Leveled up {userData.Level} and gained {exp} exp {credit} credit");
+                _log.LogAction(LogLevel.Information,
+                    $"(Exp Service | Server) {userData.UserId} Leveled up {userData.Level} and gained {exp} exp {credit} credit");
             }
             else if (userData.Exp + exp < 0)
             {
                 userData.Level -= 1;
                 userData.Exp = userData.Exp + ExpToNextLevel(userData.Level - 1) + exp;
-                _log.LogAction(LogLevel.Information, $"(Exp Service | Server) {userData.UserId} de-leveled to {userData.Level} and gained {exp} exp {credit} credit");
+                _log.LogAction(LogLevel.Information,
+                    $"(Exp Service | Server) {userData.UserId} de-leveled to {userData.Level} and gained {exp} exp {credit} credit");
             }
             else
             {
                 userData.Exp += exp;
-                _log.LogAction(LogLevel.Information, $"(Exp Service | Server) {userData.UserId} gained {exp} exp {credit} credit");
+                _log.LogAction(LogLevel.Information,
+                    $"(Exp Service | Server) {userData.UserId} gained {exp} exp {credit} credit");
             }
 
             userData.TotalExp += exp;
             userData.Credit += credit;
-            if(!userData.Active) userData.Active = true;
+            if (!userData.Active) userData.Active = true;
             await db.SaveChangesAsync();
         }
 
@@ -53,12 +68,14 @@ namespace Hanekawa.Bot.Services.Experience
             {
                 userData.Level += 1;
                 userData.Exp = userData.Exp + exp - ExpToNextLevel(userData);
-                _log.LogAction(LogLevel.Information, $"(Exp Service | Global) {userData.UserId} Leveled up {userData.Level} and gained {exp} exp {credit} credit");
+                _log.LogAction(LogLevel.Information,
+                    $"(Exp Service | Global) {userData.UserId} Leveled up {userData.Level} and gained {exp} exp {credit} credit");
             }
             else
             {
                 userData.Exp += exp;
-                _log.LogAction(LogLevel.Information, $"(Exp Service | Global) {userData.UserId} gained {exp} exp {credit} credit");
+                _log.LogAction(LogLevel.Information,
+                    $"(Exp Service | Global) {userData.UserId} gained {exp} exp {credit} credit");
             }
 
             userData.TotalExp += exp;
@@ -67,11 +84,18 @@ namespace Hanekawa.Bot.Services.Experience
         }
 
         public void AdjustTextMultiplier(ulong guildId, double multiplier)
-            => _textExpMultiplier.AddOrUpdate(guildId, multiplier, (key, value) => multiplier);
+        {
+            _textExpMultiplier.AddOrUpdate(guildId, multiplier, (key, value) => multiplier);
+        }
 
         public void AdjustVoiceMultiplier(ulong guildId, double multiplier)
-            => _voiceExpMultiplier.AddOrUpdate(guildId, multiplier, (key, value) => multiplier);
+        {
+            _voiceExpMultiplier.AddOrUpdate(guildId, multiplier, (key, value) => multiplier);
+        }
 
-        public double GetMultiplier(ulong guildId) => _textExpMultiplier.GetOrAdd(guildId, 1);
+        public double GetMultiplier(ulong guildId)
+        {
+            return _textExpMultiplier.GetOrAdd(guildId, 1);
+        }
     }
 }
