be43607943b840730ae567dc2e32ddb18b05bf14
Sphexator <kristoffer.tunlid@gmail.com>
2020-09-02 00:23:34 +0200

    Added new migration

---
+++ Hanekawa.Database/Migrations/20200901214406_self-assignable-rework.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hanekawa.Database.Migrations
+{
+    public partial class selfassignablerework : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmoteMessageFormat",
+                table: "SelfAssignAbleRoles",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmoteReactFormat",
+                table: "SelfAssignAbleRoles",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "SelfAssignableChannel",
+                table: "ChannelConfigs",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long[]>(
+                name: "SelfAssignableMessages",
+                table: "ChannelConfigs",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmoteMessageFormat",
+                table: "SelfAssignAbleRoles");
+
+            migrationBuilder.DropColumn(
+                name: "EmoteReactFormat",
+                table: "SelfAssignAbleRoles");
+
+            migrationBuilder.DropColumn(
+                name: "SelfAssignableChannel",
+                table: "ChannelConfigs");
+
+            migrationBuilder.DropColumn(
+                name: "SelfAssignableMessages",
+                table: "ChannelConfigs");
+        }
+    }
+}
