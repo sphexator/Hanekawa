8ea014b3b396ed2d0e01a65fa23059d083289b42
Sphexator <kristoffer.tunlid@gmail.com>
2020-09-06 19:37:48 +0200

    Add logging into advert endpoint

--- Hanekawa/Controllers/AdvertController.cs
+++ Hanekawa/Controllers/AdvertController.cs
@@ -3,14 +3,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Disqord;
+using Hanekawa.Bot.Services;
 using Hanekawa.Bot.Services.Experience;
 using Hanekawa.Database;
 using Hanekawa.Database.Extensions;
 using Hanekawa.Database.Tables.Advertise;
 using Hanekawa.Models;
 using Hanekawa.Utility;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Quartz.Util;
 
 namespace Hanekawa.Controllers
@@ -22,6 +23,7 @@ namespace Hanekawa.Controllers
         private readonly DbService _db;
         private readonly Bot.Hanekawa _client;
         private readonly ExpService _exp;
+        private readonly InternalLogService _log;
         public AdvertController(DbService db, Bot.Hanekawa client, ExpService exp)
         {
             _db = db;
@@ -32,72 +34,80 @@ namespace Hanekawa.Controllers
         [HttpPost("dbl")]
         public async Task<IActionResult> Dsl([FromBody] DslWebhook model)
         {
-            // Check if header has right agent
-            if (!Request.Headers.TryGetValue("User-Agent", out var agent)) return BadRequest();
-            if (!agent.Contains("DBL")) return BadRequest(); // If not send bad request, only accepting DBL user agents
-            // Check if user has a authorization in the header, else return forbidden, we only accept requests with an authorization in the header
-            if (!Request.Headers.TryGetValue("Authorization", out var authCode)) return Forbid("No authorization header");
-            var guildId = Convert.ToUInt64(model.Guild);
-            var cfg = await _db.DblAuths.FindAsync(guildId); // Get the key from database
-            // If there's no config, the guild doesn't have it enabled
-            if (cfg == null) return BadRequest();
-            // Make sure the key is correct
-            if (cfg.AuthKey.ToString() != authCode.ToString()) return Unauthorized("Invalid key");
-
-            var guild = _client.GetGuild(guildId); // Get guild and check if bot is in guild, could be kicked
-            if (guild == null) return BadRequest("Invalid guild");
-            // Get user data and reward from config
-            var userId = Convert.ToUInt64(model.User);
-            var userData = await _db.GetOrCreateUserData(guildId, userId);
-            var user = _client.GetGuild(guildId).GetMember(userId);
-            await _exp.AddExpAsync(user, userData, cfg.ExpGain, cfg.CreditGain, _db);
-            userData.CreditSpecial += cfg.SpecialCredit; // Manually add as AddExp doesn't do special credit, maybe add later?
-           
-            if (cfg.RoleIdReward.HasValue && !user.Roles.ContainsKey(cfg.RoleIdReward.Value)) // Reward a role if its in the config and the user doesn't already have it
-                await user.GrantRoleAsync(cfg.RoleIdReward.Value);
-            // Add a log entry for the vote to keep track of votes
-            await _db.VoteLogs.AddAsync(new VoteLog
-            {
-                GuildId = guildId,
-                UserId = userId,
-                Type = model.Type,
-                Time = DateTimeOffset.UtcNow
-            });
-            await _db.SaveChangesAsync();
-            
-            if (cfg.Message.IsNullOrWhiteSpace()) return Accepted(); // Check if there's a message to be sent, else we good
             try
             {
-                var str = new StringBuilder();
-                if (cfg.ExpGain > 0) str.AppendLine($"{cfg.ExpGain} Exp");
-                if (cfg.CreditGain > 0) str.AppendLine($"{cfg.CreditGain} Credit");
-                if (cfg.SpecialCredit > 0) str.AppendLine($"{cfg.SpecialCredit} Special Credit");
-                if (user.DmChannel != null
-                ) // determine if dm channel is already created, else create it and send message
-                    await user.DmChannel.SendMessageAsync(
-                        $"{MessageUtil.FormatMessage(cfg.Message, user, user.Guild)}\n" +
-                        "You've been rewarded:\n" +
-                        $"{cfg.ExpGain} Exp\n" +
-                        $"{cfg.CreditGain} Credit\n" +
-                        $"{cfg.SpecialCredit} Special Credit", false,
-                        null,
-                        LocalMentions.None);
-                else
-                {
-                    var channel = await user.CreateDmChannelAsync();
-                    await channel.SendMessageAsync($"{MessageUtil.FormatMessage(cfg.Message, user, user.Guild)}\n" +
-                                                   "You've been rewarded:\n" +
-                                                   $"{str}", false,
-                        null,
-                        LocalMentions.None);
-                }
-            }
-            catch
-            {
-                // Ignore, the user likely has closed DMs
-            }
+                // Check if header has right agent
+                if (!Request.Headers.TryGetValue("User-Agent", out var agent)) return BadRequest();
+                if (!agent.Contains("DBL")) return BadRequest(); // If not send bad request, only accepting DBL user agents
+                                                                 // Check if user has a authorization in the header, else return forbidden, we only accept requests with an authorization in the header
+                if (!Request.Headers.TryGetValue("Authorization", out var authCode)) return Unauthorized("No authorization header");
+                var guildId = Convert.ToUInt64(model.Guild);
+                var cfg = await _db.DblAuths.FindAsync(guildId); // Get the key from database
+                                                                 // If there's no config, the guild doesn't have it enabled
+                if (cfg == null) return BadRequest();
+                // Make sure the key is correct
+                if (cfg.AuthKey.ToString() != authCode.ToString()) return Unauthorized("Invalid key");
 
-            return Accepted();
+                var guild = _client.GetGuild(guildId); // Get guild and check if bot is in guild, could be kicked
+                if (guild == null) return BadRequest("Invalid guild");
+                // Get user data and reward from config
+                var userId = Convert.ToUInt64(model.User);
+                var userData = await _db.GetOrCreateUserData(guildId, userId);
+                var user = _client.GetGuild(guildId).GetMember(userId);
+                await _exp.AddExpAsync(user, userData, cfg.ExpGain, cfg.CreditGain, _db);
+                userData.CreditSpecial += cfg.SpecialCredit; // Manually add as AddExp doesn't do special credit, maybe add later?
+
+                if (cfg.RoleIdReward.HasValue && !user.Roles.ContainsKey(cfg.RoleIdReward.Value)) // Reward a role if its in the config and the user doesn't already have it
+                    await user.GrantRoleAsync(cfg.RoleIdReward.Value);
+                // Add a log entry for the vote to keep track of votes
+                await _db.VoteLogs.AddAsync(new VoteLog
+                {
+                    GuildId = guildId,
+                    UserId = userId,
+                    Type = model.Type,
+                    Time = DateTimeOffset.UtcNow
+                });
+                await _db.SaveChangesAsync();
+
+                if (cfg.Message.IsNullOrWhiteSpace()) return Accepted(); // Check if there's a message to be sent, else we good
+                try
+                {
+                    var str = new StringBuilder();
+                    if (cfg.ExpGain > 0) str.AppendLine($"{cfg.ExpGain} Exp");
+                    if (cfg.CreditGain > 0) str.AppendLine($"{cfg.CreditGain} Credit");
+                    if (cfg.SpecialCredit > 0) str.AppendLine($"{cfg.SpecialCredit} Special Credit");
+                    if (user.DmChannel != null
+                    ) // determine if dm channel is already created, else create it and send message
+                        await user.DmChannel.SendMessageAsync(
+                            $"{MessageUtil.FormatMessage(cfg.Message, user, user.Guild)}\n" +
+                            "You've been rewarded:\n" +
+                            $"{cfg.ExpGain} Exp\n" +
+                            $"{cfg.CreditGain} Credit\n" +
+                            $"{cfg.SpecialCredit} Special Credit", false,
+                            null,
+                            LocalMentions.None);
+                    else
+                    {
+                        var channel = await user.CreateDmChannelAsync();
+                        await channel.SendMessageAsync($"{MessageUtil.FormatMessage(cfg.Message, user, user.Guild)}\n" +
+                                                       "You've been rewarded:\n" +
+                                                       $"{str}", false,
+                            null,
+                            LocalMentions.None);
+                    }
+                }
+                catch
+                {
+                    // Ignore, the user likely has closed DMs
+                }
+
+                return Accepted();
+            }
+            catch (Exception e)
+            {
+                _log.LogAction(LogLevel.Error, e, $"(Advert Endpoint) Error in awarding user for voting - {e.Message}");
+                return StatusCode(500);
+            }
         }
     }
 }
