db4a91d2dfc623c238cd546931d8f8bb4a4ee60a
Sphexator <kristoffer.tunlid@gmail.com>
2020-09-21 01:14:30 +0200

    Nearly completed auto message feature

---
+++ Hanekawa/Bot/Modules/Utility/AutoMessage.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading.Tasks;
+using Disqord;
+using Disqord.Bot;
+using Hanekawa.Bot.Services.AutoMessage;
+using Hanekawa.Database;
+using Hanekawa.Shared.Command;
+using Hanekawa.Shared.Command.Extensions;
+using Humanizer;
+using Microsoft.Extensions.DependencyInjection;
+using Qmmands;
+
+namespace Hanekawa.Bot.Modules.Utility
+{
+    [Name("Auto Message")]
+    [Description("Create a message that'll be posted automatically within a interval (ie. every hour)")]
+    [RequireMemberGuildPermissions(Permission.ManageGuild)]
+    public class AutoMessage : HanekawaCommandModule
+    {
+        private readonly AutoMessageService _service;
+        public AutoMessage(AutoMessageService service) => _service = service;
+
+        [Name("Add Auto Message")]
+        [Description("Create a automated message with a interval (ie. 1h30m). You can have up to 3 active messages")]
+        [Command("amadd")]
+        public async Task AddAsync(string name, CachedTextChannel channel, TimeSpan interval, [Remainder] string message)
+        {
+            if (await _service.AddAutoMessage(Context.Member, channel, interval, name, message))
+                await ReplyAsync(
+                    $"Succesfully added a message by name '{name}' that'll be sent every {interval.Humanize()}",
+                    Color.Green);
+            else
+                await ReplyAsync(
+                    $"Couldn't add, you possibly already have 3 active messages, or there's already a message with this name ({name})",
+                    Color.Red);
+        }
+
+        [Name("Remove Auto Message")]
+        [Description("Removes a automated message by its name")]
+        [Command("amremove")]
+        public async Task RemoveAsync(string name)
+        {
+            await using var db = Context.Scope.ServiceProvider.GetRequiredService<DbService>();
+            if (_service.RemoveAutoMessage(Context.Guild.Id.RawValue, name, db))
+                await ReplyAsync($"Removed auto message by the name {name} !", Color.Green);
+            else await ReplyAsync($"Couldn't find or remove a auto message by the name {name}", Color.Green);
+        }
+
+        [Name("List Auto Messages")]
+        [Description("List all active automated messages (up to 3)")]
+        [Command("amlist")]
+        public async Task ListAsync()
+        {
+            var list = _service.GetList(Context.Guild.Id.RawValue);
+            await Context.PaginatedReply(list, Context.Guild,
+                $"Automated messages in {Context.Guild.Name}!");
+        }
+
+        [Name("Edit existing message with different message")]
+        [Description("Edit the content of a automated message by providing a new one")]
+        [Command("amedit")]
+        public async Task EditContentAsync(string name, [Remainder] string editedMessage)
+        {
+            await using var db = Context.Scope.ServiceProvider.GetRequiredService<DbService>();
+            await _service.EditMessageAsync(Context.Guild.Id.RawValue, name, editedMessage, db);
+            await ReplyAsync($"Changed message to '{editedMessage}' !", Color.Green);
+        }
+
+        [Name("Edit existing message with different interval")]
+        [Description("Change the interval a message is being posted, ie. from 1hr to 3hrs")]
+        [Command("amedit")]
+        public async Task EditedIntervalAsync(string name, TimeSpan editedInterval)
+        {
+            await using var db = Context.Scope.ServiceProvider.GetRequiredService<DbService>();
+            await _service.EditIntervalAsync(Context.Guild.Id.RawValue, name, editedInterval, db);
+            await ReplyAsync($"Changed message interval to '{editedInterval.Humanize()}' !", Color.Green);
+        }
+    }
+}
--- Hanekawa/Bot/Services/AutoMessage/AutoMessageService.cs
+++ Hanekawa/Bot/Services/AutoMessage/AutoMessageService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Disqord;
@@ -9,8 +11,10 @@ using Hanekawa.Database;
 using Hanekawa.Extensions.Embed;
 using Hanekawa.Shared.Command;
 using Hanekawa.Shared.Interfaces;
+using Hanekawa.Utility;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Hanekawa.Bot.Services.AutoMessage
 {
@@ -19,13 +23,15 @@ namespace Hanekawa.Bot.Services.AutoMessage
         private readonly Hanekawa _client;
         private readonly IServiceProvider _provider;
         private readonly ColourService _colour;
+        private readonly InternalLogService _log;
         private readonly ConcurrentDictionary<ulong, ConcurrentDictionary<string, Timer>> _timers = new ConcurrentDictionary<ulong, ConcurrentDictionary<string, Timer>>();
 
-        public AutoMessageService(Hanekawa client, IServiceProvider provider, ColourService colour)
+        public AutoMessageService(Hanekawa client, IServiceProvider provider, ColourService colour, InternalLogService log)
         {
             _client = client;
             _provider = provider;
             _colour = colour;
+            _log = log;
 
             _client.LeftGuild += ClearMessages;
 
@@ -65,7 +71,12 @@ namespace Hanekawa.Bot.Services.AutoMessage
                 if (!_timers.TryGetValue(e.Guild.Id.RawValue, out var timers)) return;
                 foreach (var x in timers.Values)
                 {
-                    await x.DisposeAsync();
+                    try
+                    {
+                        await x.DisposeAsync();
+                    }
+                    catch 
+                    { /* IGNORE */}
                 }
 
                 _timers.TryRemove(e.Guild.Id.RawValue, out _);
@@ -83,11 +94,7 @@ namespace Hanekawa.Bot.Services.AutoMessage
             var timers = _timers.GetOrAdd(user.Guild.Id.RawValue, new ConcurrentDictionary<string, Timer>());
             if (timers.Count >= 3) return false;
             var firstPost = new TimeSpan(0, (60 - DateTime.UtcNow.Minute), 0);
-            var timer = new Timer(async _ =>
-            {
-                await channel.SendMessageAsync(null, false,
-                    new LocalEmbedBuilder().Create(message, _colour.Get(user.Guild.Id.RawValue)).Build());
-            }, null, firstPost, interval);
+            var timer = CreateTimer(channel, firstPost, interval, message, user.Guild.Id.RawValue, name);
             if (timers.TryAdd(name, timer))
             {
                 await AddToDatabase(user, channel, interval, name, message);
@@ -97,44 +104,161 @@ namespace Hanekawa.Bot.Services.AutoMessage
             return false;
         }
 
-        public void RemoveAutoMessage(ulong guildId, string name, DbService db)
+        public bool RemoveAutoMessage(ulong guildId, string name, DbService db)
         {
-            if (!_timers.TryGetValue(guildId, out var timers)) return;
-            timers.TryRemove(name, out var timer);
+            if (!_timers.TryGetValue(guildId, out var timers)) return false;
+            if (!timers.TryRemove(name, out var timer)) return false;
             timer?.Dispose();
-            RemoveFromDb(guildId, name, db);
+            return RemoveFromDb(guildId, name, db);
         }
+
+        public List<string> GetList(ulong guildId)
+        {
+            var timers = _timers.GetOrAdd(guildId, new ConcurrentDictionary<string, Timer>());
+            var result = new List<string>();
+            if (timers.IsEmpty) return null;
+            foreach (var (name, timer) in timers)
+            {
+                var sb = new StringBuilder();
+                sb.AppendLine($"Name: {name}\n" +
+                              $"Interval: {timer}");
+                sb.AppendLine();
+                result.Add(sb.ToString());
+            }
+            return result;
+        }
+
+        public async Task<bool> EditMessageAsync(ulong guildId, string name, string newMessage, DbService db)
+        {
+            try
+            {
+                var timers = _timers.GetOrAdd(guildId, new ConcurrentDictionary<string, Timer>());
+                if (!timers.TryGetValue(name, out var timer)) return false;
+                var dbTimer = await db.AutoMessages.FindAsync(guildId, name);
+                if (dbTimer == null)
+                {
+                    await timer.DisposeAsync();
+                    return false;
+                }
+
+                dbTimer.Message = newMessage;
+                var firstPost = new TimeSpan(0, (60 - DateTime.UtcNow.Minute), 0);
+                var newTimer = CreateTimer(_client.GetGuild(guildId).GetTextChannel(dbTimer.ChannelId), firstPost,
+                    dbTimer.Interval, newMessage, guildId, name);
+                timers.AddOrUpdate(name, newTimer, (s, _) => newTimer);
+                await db.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception e)
+            {
+                _log.LogAction(LogLevel.Error, e, $"(Auto Message Service) Couldn't edit message for existing timer for {guildId}");
+                return false;
+            }
+        }
+
+        public async Task<bool> EditIntervalAsync(ulong guildId, string name, TimeSpan newInterval, DbService db)
+        {
+            var timers = _timers.GetOrAdd(guildId, new ConcurrentDictionary<string, Timer>());
+            if (!timers.TryGetValue(name, out var timer)) return false;
+            var dbTimer = await db.AutoMessages.FindAsync(guildId, name);
+            if (dbTimer == null)
+            {
+                await timer.DisposeAsync();
+                return false;
+            }
+
+            dbTimer.Interval = newInterval;
+            var firstPost = new TimeSpan(0, (60 - DateTime.UtcNow.Minute), 0);
+            var change = timer.Change(firstPost, newInterval);
+            if (change) await db.SaveChangesAsync();
+            return change;
+        }
+
+        private Timer CreateTimer(CachedTextChannel channel, TimeSpan firstPost, TimeSpan interval, string message,
+            ulong guildId, string name) =>
+            new Timer(async _ =>
+            {
+                var timers = _timers.GetOrAdd(guildId, new ConcurrentDictionary<string, Timer>());
+                var guild = _client.GetGuild(guildId);
+                if (guild == null)
+                {
+                    timers.TryRemove(name, out var toDispose);
+                    if (toDispose != null) await toDispose.DisposeAsync();
+                    RemoveFromDb(guildId, name);
+                    return;
+                }
+
+                var txt = guild.GetTextChannel(channel.Id.RawValue);
+                if (txt == null)
+                {
+                    timers.TryRemove(name, out var toDispose);
+                    if (toDispose != null) await toDispose.DisposeAsync();
+                    RemoveFromDb(guildId, name);
+                    return;
+                }
+
+                await channel.SendMessageAsync(null, false,
+                    new LocalEmbedBuilder().Create(MessageUtil.FormatMessage(message, null, txt.Guild), _colour.Get(guildId)).Build());
+            }, null, new TimeSpan(0, (60 - DateTime.UtcNow.Minute), 0), interval);
 
         private async Task AddToDatabase(CachedMember user, CachedTextChannel channel, TimeSpan interval, string name, string message)
         {
-            using var scope = _provider.CreateScope();
-            await using var db = scope.ServiceProvider.GetRequiredService<DbService>();
-            await db.AutoMessages.AddAsync(new Database.Tables.AutoMessage.AutoMessage
+            try
             {
-                GuildId = user.Guild.Id.RawValue,
-                Creator = user.Id.RawValue,
-                ChannelId = channel.Id.RawValue,
-                Name = name,
-                Message = message,
-                Interval = interval
-            });
-            await db.SaveChangesAsync();
+                using var scope = _provider.CreateScope();
+                await using var db = scope.ServiceProvider.GetRequiredService<DbService>();
+                await db.AutoMessages.AddAsync(new Database.Tables.AutoMessage.AutoMessage
+                {
+                    GuildId = user.Guild.Id.RawValue,
+                    Creator = user.Id.RawValue,
+                    ChannelId = channel.Id.RawValue,
+                    Name = name,
+                    Message = message,
+                    Interval = interval
+                });
+                await db.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                _log.LogAction(LogLevel.Error, e, $"(Auto Message Service) Couldn't save auto message to database for {user.Guild.Id.RawValue}");
+                throw;
+            }
         }
 
-        private void RemoveFromDb(ulong guildId, string name, DbService db)
+        private bool RemoveFromDb(ulong guildId, string name, DbService db)
         {
-            var toRemove = db.AutoMessages.Find(guildId, name);
-            db.AutoMessages.Remove(toRemove);
-            db.SaveChanges();
+            try
+            {
+                var toRemove = db.AutoMessages.Find(guildId, name);
+                if (toRemove == null) return false;
+                db.AutoMessages.Remove(toRemove);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                _log.LogAction(LogLevel.Error, e, $"(Auto Message Service) Couldn't remove {name} from {guildId}");
+                return false;
+            }
         }
 
-        private void RemoveFromDb(ulong guildId, string name)
+        private bool RemoveFromDb(ulong guildId, string name)
         {
-            using var scope = _provider.CreateScope();
-            using var db = scope.ServiceProvider.GetRequiredService<DbService>();
-            var toRemove = db.AutoMessages.Find(guildId, name);
-            db.AutoMessages.Remove(toRemove);
-            db.SaveChanges();
+            try
+            {
+                using var scope = _provider.CreateScope();
+                using var db = scope.ServiceProvider.GetRequiredService<DbService>();
+                var toRemove = db.AutoMessages.Find(guildId, name);
+                if (toRemove == null) return false;
+                db.AutoMessages.Remove(toRemove);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                _log.LogAction(LogLevel.Error, e, $"(Auto Message Service) Couldn't remove {name} from {guildId}");
+                return false;
+            }
         }
     }
 }
