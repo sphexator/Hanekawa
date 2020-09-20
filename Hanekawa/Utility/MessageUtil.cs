f4121fce8d0f587ddb7259a29fb68cc98c5cc6fc
Sphexator <kristoffer.tunlid@gmail.com>
2020-09-21 01:13:21 +0200

    Added more options to message formating (use usernames instead of mentions for welcome messages as an example)

--- Hanekawa/Utility/MessageUtil.cs
+++ Hanekawa/Utility/MessageUtil.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 using Disqord;
 using Quartz.Util;
 
@@ -15,14 +11,19 @@ namespace Hanekawa.Utility
         private static Regex ServerRegex => new Regex("%SERVER%", RegexOptions.IgnoreCase | RegexOptions.Compiled);
         private static Regex MembersRegex => new Regex("%MEMBERS%", RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
-        public static string FormatMessage(string msg, IMentionable user, CachedGuild guild)
+        private static Regex UsernameRegex => new Regex("%USERNAME%", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        private static Regex MentionRegex => new Regex("%MENTION%", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        public static string FormatMessage(string msg, CachedMember user, CachedGuild guild)
         {
             if (msg.IsNullOrWhiteSpace()) return null;
-            if (PlayerRegex.IsMatch(msg)) msg = PlayerRegex.Replace(msg, user.Mention);
-            if (UserRegex.IsMatch(msg)) msg = UserRegex.Replace(msg, user.Mention);
-            if (ServerRegex.IsMatch(msg)) msg = ServerRegex.Replace(msg, guild.Name);
-            if (MembersRegex.IsMatch(msg)) msg = MembersRegex.Replace(msg, $"{guild.MemberCount + 1}");
+            if (PlayerRegex.IsMatch(msg) && user != null) msg = PlayerRegex.Replace(msg, user.Mention);
+            if (UserRegex.IsMatch(msg) && user != null) msg = UserRegex.Replace(msg, user.Mention);
 
+            if (UsernameRegex.IsMatch(msg) && user != null) msg = UsernameRegex.Replace(msg, user.DisplayName);
+            if (MentionRegex.IsMatch(msg) && user != null) msg = MentionRegex.Replace(msg, user.Mention);
+            if (ServerRegex.IsMatch(msg) && guild != null) msg = ServerRegex.Replace(msg, guild.Name);
+            if (MembersRegex.IsMatch(msg) && guild != null) msg = MembersRegex.Replace(msg, $"{guild.MemberCount + 1}");
             return msg;
         }
     }
