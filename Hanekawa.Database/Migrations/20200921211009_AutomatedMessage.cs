26ed8345a32b08f12034c4bffd27e16279d9f9c6
Sphexator <kristoffer.tunlid@gmail.com>
2020-09-21 23:44:00 +0200

    database migration created for automessage

---
+++ Hanekawa.Database/Migrations/20200921211009_AutomatedMessage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hanekawa.Database.Migrations
+{
+    public partial class AutomatedMessage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AutoMessages",
+                columns: table => new
+                {
+                    GuildId = table.Column<long>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Creator = table.Column<long>(nullable: false),
+                    ChannelId = table.Column<long>(nullable: false),
+                    Message = table.Column<string>(nullable: true),
+                    Interval = table.Column<TimeSpan>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AutoMessages", x => new { x.GuildId, x.Name });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AutoMessages");
+        }
+    }
+}
