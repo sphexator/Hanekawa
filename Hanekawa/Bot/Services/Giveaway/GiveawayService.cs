67335e42f82affc3e76dbc0b597375e68887b97e
Sphexator <kristoffer.tunlid@gmail.com>
2020-10-11 16:53:33 +0200

    Added giveaway service

---
+++ Hanekawa/Bot/Services/Giveaway/GiveawayService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Disqord;
+using Hanekawa.Database;
+using Hanekawa.Shared;
+using Hanekawa.Shared.Interfaces;
+
+namespace Hanekawa.Bot.Services.Giveaway
+{
+    public class GiveawayService : INService
+    {
+        public async Task<bool> ActiveGiveaway(CachedMember user, DbService db, GiveawayType type)
+        {
+
+            return false;
+        }
+    }
+}
