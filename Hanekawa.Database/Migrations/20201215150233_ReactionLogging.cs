47ef7373238a255d81086d99442386269d75118f
sphexator <kristoffer.tunlid@gmail.com>
2020-12-15 16:04:07 +0100

    Added DB migration

---
+++ Hanekawa.Database/Migrations/20201215150233_ReactionLogging.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hanekawa.Database.Migrations
+{
+    public partial class ReactionLogging : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "LogReaction",
+                table: "LoggingConfigs",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReactionWebhook",
+                table: "LoggingConfigs",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LogReaction",
+                table: "LoggingConfigs");
+
+            migrationBuilder.DropColumn(
+                name: "ReactionWebhook",
+                table: "LoggingConfigs");
+        }
+    }
+}
