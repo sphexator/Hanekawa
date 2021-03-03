cd1404e0486444fb9bc059a512e2849bf010f255
Sphexator <kristoffer.tunlid@gmail.com>
2021-03-03 20:17:40 +0100

    fixes and error logging added to decay

--- Hanekawa/Bot/Services/Experience/Decay.cs
+++ Hanekawa/Bot/Services/Experience/Decay.cs
@@ -5,6 +5,7 @@ using Hanekawa.Database;
 using Hanekawa.Database.Tables.Account;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Quartz;
 
 namespace Hanekawa.Bot.Services.Experience
@@ -19,35 +20,42 @@ namespace Hanekawa.Bot.Services.Experience
 
         private async Task DecayAsync()
         {
-            using var scope = _provider.CreateScope();
-            await using var db = scope.ServiceProvider.GetRequiredService<DbService>();
-            var limit = DateTime.UtcNow.AddDays(-14);
-            var servers = await db.LevelConfigs.Where(x => x.Decay).ToArrayAsync();
-            if (servers.Length <= 0) return;
-            foreach (var x in servers)
+            try
             {
-                var server = _client.GetGuild(x.GuildId);
-                if(server == null) continue;
-                var levelRewards = await db.LevelRewards.Where(e => e.GuildId == x.GuildId).ToListAsync();
-                var users = await db.Accounts.Where(e =>
-                    e.GuildId == x.GuildId && e.Active && e.LastMessage <= limit && e.Decay < e.TotalExp).ToArrayAsync();
-                foreach (var user in users)
+                using var scope = _provider.CreateScope();
+                await using var db = scope.ServiceProvider.GetRequiredService<DbService>();
+                var limit = DateTime.UtcNow.AddDays(-14);
+                var servers = await db.LevelConfigs.Where(x => x.Decay).ToArrayAsync();
+                if (servers.Length <= 0) return;
+                foreach (var x in servers)
                 {
-                    if(user.TotalExp == user.Decay) continue;
-                    var member = server.GetMember(user.UserId);
-                    if (member == null)
+                    var server = _client.GetGuild(x.GuildId);
+                    if (server == null) continue;
+                    var levelRewards = await db.LevelRewards.Where(e => e.GuildId == x.GuildId).ToListAsync();
+                    var users = await db.Accounts.Where(e =>
+                        e.GuildId == x.GuildId && e.Active && e.LastMessage <= limit && e.Decay < e.TotalExp).ToArrayAsync();
+                    foreach (var user in users)
                     {
-                        user.Active = false;
-                        continue;
+                        if (user.TotalExp == user.Decay) continue;
+                        var member = server.GetMember(user.UserId);
+                        if (member == null)
+                        {
+                            user.Active = false;
+                            continue;
+                        }
+                        if (member.IsBoosting) continue;
+                        var decay = Convert.ToInt32((limit - user.LastMessage).TotalDays) * 1000;
+                        if (decay == user.Decay) continue;
+                        if (user.TotalExp <= user.Decay + decay) user.Decay = user.TotalExp;
+                        if (user.TotalExp > user.Decay + decay) user.Decay = decay;
+                        await RoleCheckAsync(member, x, user, levelRewards, db, LevelDecay(user, decay));
                     }
-                    if (member.IsBoosting) continue;
-                    var decay = Convert.ToInt32((limit - user.LastMessage).TotalDays) * 1000;
-                    if(decay == user.Decay) continue;
-                    if (user.TotalExp <= user.Decay + decay) user.Decay = user.TotalExp;
-                    if (user.TotalExp > user.Decay + decay) user.Decay = decay;
-                    await RoleCheckAsync(member, x, user, levelRewards, db, LevelDecay(user, decay));
+                    await db.SaveChangesAsync();
                 }
-                await db.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                _log.LogAction(LogLevel.Error, e, $"(Exp Service: Decay) Error occured during decay - {e.Message}");
             }
         }
 
