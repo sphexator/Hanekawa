91f7f14286069bea01b11e9be68650ce449460ce
Sphexator <kristoffer.tunlid@gmail.com>
2021-04-03 02:39:39 +0200

    Added range typereader from a min and max value

---
+++ Hanekawa/Bot/TypeReaders/RangeTypeReader.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Qmmands;
+
+namespace Hanekawa.Bot.TypeReaders
+{
+    public class RangeTypeReader : TypeParser<Models.Range>
+    {
+        public override ValueTask<TypeParserResult<Models.Range>> ParseAsync(Parameter parameter, string value, CommandContext context)
+        {
+            var result = value.Split('-');
+            int minValue = int.MaxValue;
+            int maxValue = 0;
+            foreach (var x in result)
+            {
+                if (!int.TryParse(x, out var val)) return TypeParserResult<Models.Range>.Unsuccessful("Failed to parse range");
+
+                if (val > maxValue) maxValue = val;
+                if (val < minValue) minValue = val;
+            }
+
+            return TypeParserResult<Models.Range>.Successful(new Models.Range(minValue, maxValue));
+        }
+    }
+}
---
+++ Hanekawa/Models/Range.cs
@@ -0,0 +1,14 @@
+﻿namespace Hanekawa.Models
+{
+    public class Range
+    {
+        public Range(int minValue, int maxValue)
+        {
+            MaxValue = maxValue;
+            MinValue = minValue;
+        }
+
+        public int MinValue { get; }
+        public int MaxValue { get; }
+    }
+}
