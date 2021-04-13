543d62b00f6580ebb69b52159ff8f026ed9b1e1b
Sphexator <kristoffer.tunlid@gmail.com>
2021-04-14 00:01:37 +0200

    Added invite caching for join logs and gathering all cache from DB when client is ready

--- Hanekawa/Bot/Service/EventHandler.cs
+++ Hanekawa/Bot/Service/EventHandler.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Disqord;
+using Disqord.Bot;
 using Disqord.Gateway;
 using Hanekawa.Bot.Service.Administration;
 using Hanekawa.Bot.Service.Administration.Mute;
@@ -8,16 +11,20 @@ using Hanekawa.Bot.Service.Board;
 using Hanekawa.Bot.Service.Boost;
 using Hanekawa.Bot.Service.Cache;
 using Hanekawa.Bot.Service.Drop;
+using Hanekawa.Bot.Service.Game;
 using Hanekawa.Bot.Service.Logs;
+using Hanekawa.Database;
 using Hanekawa.Entities;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
 using Quartz;
 
 namespace Hanekawa.Bot.Service
 {
-    public class EventHandler : INService, IJob
+    public class EventHandler : INService, IRequired, IJob
     {
         private readonly Hanekawa _client;
+        private readonly IServiceProvider _provider;
         private readonly CacheService _cache;
         private readonly Experience _experience;
         private readonly LogService _logService;
@@ -26,8 +33,11 @@ namespace Hanekawa.Bot.Service
         private readonly BoardService _boardService;
         private readonly BoostService _boostService;
         private readonly DropService _dropService;
+        private readonly HungerGameService _hungerGame;
 
-        public EventHandler(Hanekawa client, Experience experience, CacheService cache, LogService logService, BlacklistService blacklist, MuteService mute, BoardService boardService, BoostService boostService, DropService dropService)
+        public EventHandler(Hanekawa client, Experience experience, CacheService cache, LogService logService, 
+            BlacklistService blacklist, MuteService mute, BoardService boardService, BoostService boostService, 
+            DropService dropService, HungerGameService hungerGame, IServiceProvider provider)
         {
             _client = client;
             _experience = experience;
@@ -38,7 +48,9 @@ namespace Hanekawa.Bot.Service
             _boardService = boardService;
             _boostService = boostService;
             _dropService = dropService;
-
+            _hungerGame = hungerGame;
+            _provider = provider;
+            
             _client.MessageReceived += MessageReceived;
             _client.MessageUpdated += MessageUpdated;
             _client.MessageDeleted += MessageDeleted;
@@ -63,6 +75,54 @@ namespace Hanekawa.Bot.Service
 
             _client.JoinedGuild += JoinedGuild;
             _client.LeftGuild += LeftGuild;
+            
+            _client.InviteCreated += InviteCreated;
+            _client.InviteDeleted += InviteDeleted;
+            
+            _client.Ready += Ready;
+        }
+
+        private Task Ready(object sender, ReadyEventArgs e)
+        {
+            _ = Task.Run(async () =>
+            {
+                var scope = _provider.CreateScope();
+                await using var db = scope.ServiceProvider.GetRequiredService<DbService>();
+
+                foreach (var x in db.LevelConfigs)
+                {
+                    var expMult = _cache.ExperienceMultipliers.GetOrAdd(new Snowflake(x.GuildId),
+                        new ConcurrentDictionary<ExpSource, double>());
+                    expMult.TryAdd(ExpSource.Text, x.TextExpMultiplier);
+                    expMult.TryAdd(ExpSource.Voice, x.VoiceExpMultiplier);
+                }
+
+                foreach (var x in db.LevelExpReductions)
+                {
+                    var cache = _cache.ExperienceReduction.GetOrAdd(new Snowflake(x.GuildId), new HashSet<Snowflake>());
+                    cache.Add(x.ChannelId);
+                }
+
+                foreach (var x in db.GuildConfigs)
+                {
+                    var prefixes = _cache.GuildPrefix.GetOrAdd(new Snowflake(x.GuildId), new HashSet<IPrefix>());
+                    prefixes.Add(new StringPrefix(x.Prefix));
+                    _cache.GuildEmbedColors.TryAdd(new Snowflake(x.GuildId), new Color((int)x.EmbedColor));
+                }
+            });
+            return Task.CompletedTask;
+        }
+
+        private Task InviteDeleted(object sender, InviteDeletedEventArgs e)
+        {
+            _ = _logService.InviteDeletedAsync(e);
+            return Task.CompletedTask;
+        }
+
+        private Task InviteCreated(object sender, InviteCreatedEventArgs e)
+        {
+            _ = _logService.InviteCreatedAsync(e);
+            return Task.CompletedTask;
         }
 
         private Task MessagesDeleted(object sender, MessagesDeletedEventArgs e)
@@ -92,6 +152,7 @@ namespace Hanekawa.Bot.Service
         private Task MemberLeft(object sender, MemberLeftEventArgs e)
         {
             _ = _logService.LeaveLogAsync(e);
+            _ = _hungerGame.UserLeftAsync(e);
             return Task.CompletedTask;
         }
 
@@ -99,6 +160,7 @@ namespace Hanekawa.Bot.Service
         {
             _ = _logService.MemberUpdatedAsync(e);
             _ = _boostService.BoostCheckAsync(e);
+            _ = _hungerGame.UpdateUserAsync(e);
             return Task.CompletedTask;
         }
 
@@ -114,16 +176,6 @@ namespace Hanekawa.Bot.Service
             return Task.CompletedTask;
         }
 
-        private Task ChannelDeleted(object sender, ChannelDeletedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private Task RoleDeleted(object sender, RoleDeletedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
         private Task BanDeleted(object sender, BanDeletedEventArgs e)
         {
             _ = _logService.UnbanAsync(e);
@@ -135,10 +187,12 @@ namespace Hanekawa.Bot.Service
             _ = _logService.BanAsync(e);
             return Task.CompletedTask;
         }
-
+        
         private Task VoiceStateUpdated(object sender, VoiceStateUpdatedEventArgs e)
         {
-            throw new NotImplementedException();
+            _ = _experience.VoiceExperienceAsync(e);
+            _ = _logService.VoiceLogAsync(e);
+            return Task.CompletedTask;
         }
 
         private Task ReactionsCleared(object sender, ReactionsClearedEventArgs e)
@@ -150,6 +204,7 @@ namespace Hanekawa.Bot.Service
         private Task ReactionRemoved(object sender, ReactionRemovedEventArgs e)
         {
             _ = _boardService.ReactionRemovedAsync(e);
+            _ = _hungerGame.ReactionRemovedAsync(e);
             return Task.CompletedTask;
         }
 
@@ -158,6 +213,7 @@ namespace Hanekawa.Bot.Service
             if (e.Member.IsBot) return Task.CompletedTask;
             _ = _boardService.ReactionReceivedAsync(e);
             _ = _dropService.ReactionReceived(e);
+            _ = _hungerGame.ReactionReceivedAsync(e);
             return Task.CompletedTask;
         }
 
@@ -178,7 +234,17 @@ namespace Hanekawa.Bot.Service
             _ = _experience.GlobalExperienceAsync(e);
             return Task.CompletedTask;
         }
+        
+        private Task ChannelDeleted(object sender, ChannelDeletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
 
+        private Task RoleDeleted(object sender, RoleDeletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        
         public Task Execute(IJobExecutionContext context)
         {
             throw new NotImplementedException();
--- Hanekawa/Bot/Service/Logs/InviteLogging.cs
+++ Hanekawa/Bot/Service/Logs/InviteLogging.cs
@@ -1,11 +1,40 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using Disqord;
+using Disqord.Gateway;
+using Hanekawa.Database;
+using Hanekawa.Database.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Hanekawa.Bot.Service.Logs
 {
     public partial class LogService
     {
-        // TODO: Wait till invite events has been added
-        public async Task InviteCreatedAsync() { }
-        public async Task InviteDeletedAsync() { }
+        public async ValueTask InviteCreatedAsync(InviteCreatedEventArgs e)
+        {
+            if (!e.GuildId.HasValue) return;
+            using var scope = _provider.CreateScope();
+            await using var db = scope.ServiceProvider.GetRequiredService<DbService>();
+            var cfg = await db.GetOrCreateLoggingConfigAsync(e.GuildId.Value);
+            if (!cfg.LogJoin.HasValue) return;
+            var invites = _cache.GuildInvites.GetOrAdd(e.GuildId.Value.RawValue, new ConcurrentDictionary<string, Tuple<Snowflake?, int>>());
+            invites.TryAdd(e.Code, new Tuple<Snowflake?, int>(e.Invite.Inviter.Value.Id.RawValue, 0));
+            _cache.GuildInvites.AddOrUpdate(e.GuildId.Value.RawValue, new ConcurrentDictionary<string, Tuple<Snowflake?, int>>(),
+                (_, _) => invites);
+        }
+
+        public async ValueTask InviteDeletedAsync(InviteDeletedEventArgs e)
+        {
+            if (!e.GuildId.HasValue) return;
+            using var scope = _provider.CreateScope();
+            await using var db = scope.ServiceProvider.GetRequiredService<DbService>();
+            var cfg = await db.GetOrCreateLoggingConfigAsync(e.GuildId.Value.RawValue);
+            if (!cfg.LogJoin.HasValue) return;
+            var invites = _cache.GuildInvites.GetOrAdd(e.GuildId.Value.RawValue, new ConcurrentDictionary<string, Tuple<Snowflake?, int>>());
+            invites.TryRemove(e.Code, out _);
+            _cache.GuildInvites.AddOrUpdate(e.GuildId.Value.RawValue, new ConcurrentDictionary<string, Tuple<Snowflake?, int>>(),
+                (_, _) => invites);
+        }
     }
 }
