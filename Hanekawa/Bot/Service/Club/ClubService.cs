b56ec882a7ab977c2ce333098fb20d03ee8df488
Sphexator <kristoffer.tunlid@gmail.com>
2021-04-21 18:35:14 +0200

    Partly implement ClubService.cs

---
+++ Hanekawa/Bot/Service/Club/ClubService.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Disqord;
+using Disqord.Gateway;
+using Hanekawa.Database;
+using Hanekawa.Database.Entities;
+using Hanekawa.Database.Extensions;
+using Hanekawa.Database.Tables.Club;
+using Hanekawa.Database.Tables.Config.Guild;
+using Hanekawa.Entities;
+using Hanekawa.Extensions;
+using Microsoft.CodeAnalysis.FlowAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NLog;
+
+namespace Hanekawa.Bot.Service.Club
+{
+    public class ClubService : INService
+    {
+        private readonly Hanekawa _bot;
+        private readonly IServiceProvider _provider;
+        private readonly Logger _logger;
+        private readonly OverwritePermissions _denyOverwrite =
+            new(ChannelPermissions.None, new ChannelPermissions(19520));
+        private readonly OverwritePermissions _allowOverwrite =
+            new(new ChannelPermissions(19520), ChannelPermissions.None);
+
+        public ClubService(Hanekawa bot, IServiceProvider provider)
+        {
+            _bot = bot;
+            _provider = provider;
+            _logger = LogManager.GetCurrentClassLogger();
+        }
+
+        public async Task ReactionAddedAsync(ReactionAddedEventArgs e)
+        {
+            if (!e.GuildId.HasValue) return;
+            var guild = _bot.GetGuild(e.GuildId.Value);
+            try
+            {
+                var user = await guild.GetOrFetchMemberAsync(e.UserId);
+                using var scope = _provider.CreateScope();
+                await using var db = scope.ServiceProvider.GetRequiredService<DbService>();
+                var club = await db.ClubInfos.FirstOrDefaultAsync(x => x.AdMessage.Value == e.MessageId);
+                if (club == null) return;
+                var clubCheck =
+                    await db.ClubPlayers.FirstOrDefaultAsync(x => x.GuildId == guild.Id && x.UserId == user.Id);
+                if (clubCheck != null) return;
+                await db.ClubPlayers.AddAsync(new ClubUser
+                {
+                    Id = Guid.NewGuid(),
+                    Rank = ClubRank.Member,
+                    GuildId = guild.Id,
+                    JoinedAt = DateTimeOffset.UtcNow,
+                    UserId = user.Id,
+                    ClubId = club.Id,
+                    Club = club
+                });
+                await db.SaveChangesAsync();
+            }
+            catch (Exception exception)
+            {
+                _logger.Log(LogLevel.Error, exception,
+                    $"(Club Service) Error in {guild.Id.RawValue} for reaction added - {exception.Message}");
+            }
+        }
+
+        public async Task ReactionRemovedAsync(ReactionRemovedEventArgs e)
+        {
+            if (!e.GuildId.HasValue) return;
+            var guild = _bot.GetGuild(e.GuildId.Value);
+            try
+            {
+                var user = await guild.GetOrFetchMemberAsync(e.UserId);
+                using var scope = _provider.CreateScope();
+                await using var db = scope.ServiceProvider.GetRequiredService<DbService>();
+                var clubUser = await db.ClubPlayers.Include(x => x.Club)
+                    .FirstOrDefaultAsync(x => x.GuildId == e.GuildId && x.UserId == e.UserId);
+                if (clubUser?.Club.AdMessage == null) return;
+                if (clubUser.Club.AdMessage != null && e.MessageId != clubUser.Club.AdMessage.Value) return;
+                if (clubUser.Rank == ClubRank.Owner) return;
+                db.ClubPlayers.Remove(clubUser);
+                await db.SaveChangesAsync();
+                _logger.Log(LogLevel.Info, $"(Club Service) {user.Id.RawValue} left their club with reaction removed");
+            }
+            catch (Exception exception)
+            {
+                _logger.Log(LogLevel.Error, exception,
+                    $"(Club Service) Error in {guild.Id.RawValue} for reaction removed- {exception.Message}");
+            }
+        }
+        
+        public async Task MemberLeftAsync(MemberLeftEventArgs e)
+        {
+            var user = e.User;
+            var guild = _bot.GetGuild(e.GuildId);
+            try
+            {
+                using var scope = _provider.CreateScope();
+                await using var db = scope.ServiceProvider.GetRequiredService<DbService>();
+                var clubUser = await db.ClubPlayers.Include(x => x.Club)
+                    .FirstOrDefaultAsync(x => x.GuildId == e.GuildId && x.UserId == e.User.Id);
+                if (clubUser == null) return;
+                db.ClubPlayers.Remove(clubUser);
+                await db.SaveChangesAsync();
+                _logger.Log(LogLevel.Info, $"(Club Service) {user.Id.RawValue} left {guild.Id.RawValue} and left their club");
+            }
+            catch (Exception exception)
+            {
+                _logger.Log(LogLevel.Error, exception,
+                    $"(Club Service) Error in {guild.Id.RawValue} for User Left- {exception.Message}");
+            }
+        }
+        // TODO: Finish clubs
+    }
+}
