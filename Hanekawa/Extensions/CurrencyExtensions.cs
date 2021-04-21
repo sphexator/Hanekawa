076325bae7a2450462d70308107e3c672dc52ac5
Sphexator <kristoffer.tunlid@gmail.com>
2021-04-21 18:20:33 +0200

    Refactoring extensions

--- Hanekawa/Extensions/CurrencyExtensions.cs
+++ Hanekawa/Extensions/CurrencyExtensions.cs
@@ -1,12 +1,14 @@
 ﻿using System.Linq;
 using Disqord;
 using Hanekawa.Database.Tables.Config.Guild;
+using Hanekawa.Exceptions;
+using static Disqord.LocalCustomEmoji;
 
 namespace Hanekawa.Extensions
 {
     public static class CurrencyExtensions
     {
-        private static readonly string[] _currencySigns = {"$", "€", "£"};
+        private static readonly string[] CurrencySigns = {"$", "€", "£"};
 
         public static string ToCurrencyFormat(this CurrencyConfig cfg, int amount, bool isSpecial = false)
         {
@@ -20,15 +22,16 @@ namespace Hanekawa.Extensions
         private static string ParseEmote(string sign, int amount) => $"{amount} {CurrencySignEmote(sign)}";
 
         private static string ParseString(string sign, int amount) =>
-            _currencySigns.Contains(sign)
+            CurrencySigns.Contains(sign)
                 ? $"{sign}{amount}"
                 : $"{amount} {sign}";
 
         private static IEmoji CurrencySignEmote(string emoteString)
         {
-            if (LocalCustomEmoji.TryParse(emoteString, out var emote)) return emote;
-            LocalCustomEmoji.TryParse("<a:wawa:475462796214009856>", out var defaultEmote);
-            return defaultEmote;
+            if (TryParse(emoteString, out var emote)) return emote;
+            return TryParse("<a:wawa:475462796214009856>", out var defaultEmote)
+                ? defaultEmote
+                : throw new HanaCommandException("Default currency emote couldn't be found");
         }
     }
 }
--- Hanekawa/Extensions/NumberExtension.cs
+++ Hanekawa/Extensions/NumberExtension.cs
@@ -10,22 +10,23 @@ namespace Hanekawa.Extensions
         public static string SizeSuffix(this long value, int decimalPlaces = 1)
         {
             if (decimalPlaces < 0) throw new ArgumentOutOfRangeException(nameof(decimalPlaces));
-            if (value < 0) return "-" + SizeSuffix(-value);
-            if (value == 0) return string.Format("{0:n" + decimalPlaces + "} bytes", 0);
+            switch (value)
+            {
+                case < 0:
+                    return "-" + SizeSuffix(-value);
+                case 0:
+                    return string.Format("0:n" + decimalPlaces + " bytes", 0);
+            }
 
             var mag = (int) Math.Log(value, 1024);
 
             var adjustedSize = (decimal) value / (1L << (mag * 10));
 
-            if (Math.Round(adjustedSize, decimalPlaces) >= 1000)
-            {
-                mag += 1;
-                adjustedSize /= 1024;
-            }
-
-            return string.Format("{0:n" + decimalPlaces + "} {1}",
-                adjustedSize,
-                SizeSuffixes[mag]);
+            if (Math.Round(adjustedSize, decimalPlaces) < 1000)
+                return $"{adjustedSize} {SizeSuffixes[mag]}";
+            mag += 1;
+            adjustedSize /= 1024;
+            return $"{adjustedSize} {SizeSuffixes[mag]}";
         }
 
         public static bool IsDivisible(this int x, int n) 
