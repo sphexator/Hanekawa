05f2c3ddb68a4004c2b92dddd6b79fe95d77a45b
Sphexator <kristoffer.tunlid@gmail.com>
2021-04-21 18:37:24 +0200

    Changed command context to use interfaces instead of cached entities.

--- Hanekawa/Bot/Commands/HanekawaCommandContext.cs
+++ Hanekawa/Bot/Commands/HanekawaCommandContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Disqord;
 using Disqord.Bot;
 using Disqord.Gateway;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,17 +8,18 @@ namespace Hanekawa.Bot.Commands
 {
     public class HanekawaCommandContext : DiscordCommandContext
     {
-        public CachedMember User { get; set; }
-        public CachedGuild Guild { get; set; }
-        public CachedTextChannel Channel { get; set; }
+        public IMember User { get; set; }
+        public IGuild Guild { get; set; }
+        public ITextChannel Channel { get; set; }
         public IServiceScope Scope { get; set; }
 
         public HanekawaCommandContext(DiscordBotBase bot, IPrefix prefix, IGatewayUserMessage message, IServiceScope serviceScope) : base(bot, prefix, message, serviceScope)
         {
-            if (!(message.Author is CachedMember member))
+            if (message.Author is not IMember member)
                 throw new InvalidOperationException("Bot does not function in dms :)");
+            Scope = serviceScope;
             var guild = bot.GetGuild(member.GuildId);
-            if(!guild.Channels.TryGetValue(message.ChannelId, out var channel) || !(channel is CachedTextChannel txtChannel)) 
+            if(!guild.Channels.TryGetValue(message.ChannelId, out var channel) || channel is not ITextChannel txtChannel) 
                 throw new InvalidOperationException("Bot does not function in dms :)");
 
             User = member;
