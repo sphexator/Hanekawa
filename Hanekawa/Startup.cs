fc8dace12a3c33673a5bc1176761f853c715996f
Sphexator <kristoffer.tunlid@gmail.com>
2021-05-01 19:48:58 +0200

    Moved logging stuff from Startup.cs to Program.cs

--- Hanekawa/Startup.cs
+++ Hanekawa/Startup.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Reflection;
@@ -17,12 +16,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using NLog;
-using NLog.Config;
-using NLog.Targets;
-using NLog.Targets.Wrappers;
 using Qmmands;
-using LogLevel = NLog.LogLevel;
 
 namespace Hanekawa
 {
@@ -85,7 +79,6 @@ namespace Hanekawa
                     "default",
                     "{controller}/{action=Index}/{id?}");
             });
-            NLog.Web.NLogBuilder.ConfigureNLog(ConfigureNLog());
         }
 
         private static readonly ILoggerFactory MyLoggerFactory
@@ -97,92 +90,5 @@ namespace Hanekawa
                         && level == Microsoft.Extensions.Logging.LogLevel.Information)
                     .AddConsole();
             });
-
-        private LoggingConfiguration ConfigureNLog()
-        {
-            var consoleTarget = new ConsoleTarget
-            {
-                Name = "Console",
-                Layout = @"${longdate} | ${level} | ${message} | ${exception}",
-                DetectConsoleAvailable = true,
-                OptimizeBufferReuse = true,
-                AutoFlush = true
-            };
-            var fileTarget = new FileTarget
-            {
-                Name = "File",
-                FileName = "${basedir}/logs/${shortdate}-log.txt",
-                Layout = "${longdate} | ${level} | ${message} | ${exception}",
-                OptimizeBufferReuse = true,
-                AutoFlush = true,
-                ConcurrentWriteAttempts = -1,
-                ArchiveEvery = FileArchivePeriod.Day,
-                ArchiveOldFileOnStartup = true,
-                ConcurrentWrites = true,
-                CreateDirs = true
-            };
-            var dbTarget = new DatabaseTarget
-            {
-                Name = "Database",
-                ConnectionString = Configuration["connectionString"],
-                DBProvider = "Npgsql.NpgsqlConnection,Npgsql",
-                CommandText = @"INSERT INTO ""Logs"" " +
-                              @"(""TimeStamp"", ""Level"", ""Message"", ""Logger"", ""CallSite"", ""Exception"") " +
-                              @"VALUES " +
-                              @"(@datetime, @level, @message, @logger, @callsite, @exception)",
-                KeepConnection = true,
-                Parameters =
-                {
-                    new DatabaseParameterInfo("@datetime", "${longdate}"),
-                    new DatabaseParameterInfo("@level", "${level}"),
-                    new DatabaseParameterInfo("@message", "${message}"),
-                    new DatabaseParameterInfo("@logger", "${logger}"),
-                    new DatabaseParameterInfo("@callsite", "${callsite}"),
-                    new DatabaseParameterInfo("@exception", "${exception:format=toString,Data}")
-                },
-                OptimizeBufferReuse = true
-            };
-            var asyncConsoleTarget = new AsyncTargetWrapper
-            {
-                Name = "Async Console Target",
-                OptimizeBufferReuse = true,
-                OverflowAction = AsyncTargetWrapperOverflowAction.Grow,
-                WrappedTarget = consoleTarget,
-                TimeToSleepBetweenBatches = 1,
-                QueueLimit = 25
-            };
-
-            var asyncFileTarget = new AsyncTargetWrapper
-            {
-                Name = "Async File Target",
-                OptimizeBufferReuse = true,
-                OverflowAction = AsyncTargetWrapperOverflowAction.Grow,
-                WrappedTarget = fileTarget,
-                TimeToSleepBetweenBatches = 1,
-                QueueLimit = 25
-            };
-
-            var asyncDatabaseTarget = new AsyncTargetWrapper
-            {
-                Name = "Async Database Target",
-                OptimizeBufferReuse = true,
-                OverflowAction = AsyncTargetWrapperOverflowAction.Grow,
-                WrappedTarget = dbTarget,
-                TimeToSleepBetweenBatches = 1,
-                QueueLimit = 25
-            };
-
-            var config = new LoggingConfiguration();
-            config.AddTarget(asyncConsoleTarget);
-            config.AddTarget(asyncFileTarget);
-            config.AddTarget(asyncDatabaseTarget);
-
-            config.AddRule(LogLevel.Info, LogLevel.Fatal, asyncConsoleTarget);
-            config.AddRule(LogLevel.Warn, LogLevel.Fatal, asyncDatabaseTarget);
-            LogManager.Configuration = config;
-            LogManager.ThrowExceptions = Debugger.IsAttached;
-            
-            return config;
-        }
     }
 }
