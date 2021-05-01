27dc49ca0ecd466fcce93fae7f9bd7cc83588c1b
Sphexator <kristoffer.tunlid@gmail.com>
2021-05-01 20:02:34 +0200

    Minor change in RequirePremium.cs

--- Hanekawa/Bot/Commands/Preconditions/RequirePremium.cs
+++ Hanekawa/Bot/Commands/Preconditions/RequirePremium.cs
@@ -11,14 +11,12 @@ namespace Hanekawa.Bot.Commands.Preconditions
     {
         public override async ValueTask<CheckResult> CheckAsync(CommandContext _)
         {
-            if (_ is not HanekawaCommandContext context) return CheckResult.Failed("wrong right context.");
-            if (context.Guild == null) return CheckResult.Failed("Needs to be executed in a guild!");
-            using var scope = context.Services.CreateScope();
-            await using var db = scope.ServiceProvider.GetRequiredService<DbService>();
+            if (_ is not HanekawaCommandContext context) return CheckResult.Failed("Wrong context.");
+            await using var db = context.Scope.ServiceProvider.GetRequiredService<DbService>();
             var cfg = await db.GetOrCreateGuildConfigAsync(context.Guild);
             return cfg.Premium.HasValue && cfg.Premium.Value >= DateTimeOffset.UtcNow
                 ? CheckResult.Successful
-                : CheckResult.Failed("Command is only available for premium servers!");
+                : CheckResult.Failed("Command is only available for premium servers.");
         }
     }
 }
