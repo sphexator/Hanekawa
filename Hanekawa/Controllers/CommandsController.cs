ede4a6ed7882a13f7583e61fd67974792f3d4f84
Sphexator <kristoffer.tunlid@gmail.com>
2021-05-02 16:20:00 +0200

    Use extensions in commands controller

--- Hanekawa/Controllers/CommandsController.cs
+++ Hanekawa/Controllers/CommandsController.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using Disqord.Bot;
-using Disqord.Gateway;
-using Hanekawa.Bot.Commands.Preconditions;
+using Hanekawa.Extensions;
 using Microsoft.AspNetCore.Mvc;
-using Qmmands;
 using Command = Hanekawa.Models.Command;
 using Module = Hanekawa.Models.Module;
 
@@ -33,7 +29,7 @@ namespace Hanekawa.Controllers
                 {
                     if (commands.TryGetValue(c.Name, out var command))
                     {
-                        command.Example.Add($"{c.FullAliases.FirstOrDefault()} {ExampleParamBuilder(c)}");
+                        command.Example.Add($"{c.FullAliases.FirstOrDefault()} {c.ExampleParamBuilder()}");
                     }
                     else
                     {
@@ -43,9 +39,9 @@ namespace Hanekawa.Controllers
                             Commands = c.FullAliases.ToList(),
                             Description = c.Description,
                             Example = new List<string>
-                                {$"{c.FullAliases.FirstOrDefault()} {ExampleParamBuilder(c)}"},
-                            Premium = PremiumCheck(c),
-                            Permissions = PermBuilder(c)
+                                {$"{c.FullAliases.FirstOrDefault()} {c.ExampleParamBuilder()}"},
+                            Premium = c.PremiumCheck(out _),
+                            Permissions = c.PermBuilder()
                         });
                     }
                 }
@@ -59,65 +55,5 @@ namespace Hanekawa.Controllers
 
             return toReturn;
         }
-
-        private static bool PremiumCheck(Qmmands.Command cmd)
-        {
-            var premium = cmd.Checks.FirstOrDefault(x => x is RequirePremium) 
-                          ?? cmd.Module.Checks.FirstOrDefault(x => x is RequirePremium);
-            return premium != null;
-        }
-
-        private static List<string> PermBuilder(Qmmands.Command cmd)
-        {
-            var str = new List<string>();
-            foreach (var x in cmd.Module.Checks)
-            {
-                if (x is RequireAuthorGuildPermissionsAttribute perm)
-                {
-                    str.Add(perm.Permissions.ToString());
-                }
-            }
-            foreach (var x in cmd.Checks)
-            {
-                if (x is RequireAuthorGuildPermissionsAttribute perm)
-                { 
-                    str.Add(perm.Permissions.ToString());
-                }
-            }
-
-            return str;
-        }
-
-        private string ExampleParamBuilder(Qmmands.Command command)
-        {
-            var output = new StringBuilder();
-            if (!command.Parameters.Any()) return output.ToString();
-            foreach (var x in command.Parameters)
-            {
-                var name = PermTypeBuilder(x);
-                if (x.IsOptional)
-                    output.Append($"{name} ");
-                else if (x.IsRemainder)
-                    output.Append($"{name} ");
-                else if (x.IsMultiple)
-                    output.Append($"{name} ");
-                else
-                    output.Append($"{name} ");
-            }
-
-            return output.ToString();
-        }
-
-        private static string PermTypeBuilder(Parameter parameter) =>
-            parameter.Type == typeof(CachedMember) ? "@bob#0000" :
-            parameter.Type == typeof(CachedRole) ? "role" :
-            parameter.Type == typeof(CachedTextChannel) ? "#General" :
-            parameter.Type == typeof(CachedVoiceChannel) ? "VoiceChannel" :
-            parameter.Type == typeof(CachedCategoryChannel) ? "Category" :
-            parameter.Type == typeof(TimeSpan?) ? "1h2m" :
-            parameter.Type == typeof(TimeSpan) ? "1h2m" :
-            parameter.Type == typeof(int) ? "5" :
-            parameter.Type == typeof(string) ? "Example text" :
-            parameter.Type == typeof(ulong) ? "431610594290827267" : parameter.Name;
     }
 }
