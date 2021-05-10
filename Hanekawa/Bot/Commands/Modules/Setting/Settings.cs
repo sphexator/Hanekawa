9bc418a2fdb1feff9ded15436f6cdf653878ce4f
Sphexator <kristoffer.tunlid@gmail.com>
2021-05-10 21:23:52 +0200

    Added setting module module

--- Hanekawa/Bot/Commands/Modules/Setting/Settings.cs
+++ Hanekawa/Bot/Commands/Modules/Setting/Settings.cs
@@ -1,5 +1,12 @@
-﻿using Disqord;
+﻿using System.Threading.Tasks;
+using Disqord;
 using Disqord.Bot;
+using Disqord.Extensions.Interactivity;
+using Hanekawa.Bot.Service.Cache;
+using Hanekawa.Database;
+using Hanekawa.Database.Extensions;
+using Hanekawa.Entities.Color;
+using Microsoft.Extensions.DependencyInjection;
 using Qmmands;
 
 namespace Hanekawa.Bot.Commands.Modules.Setting
@@ -9,6 +16,64 @@ namespace Hanekawa.Bot.Commands.Modules.Setting
     [RequireBotGuildPermissions(Permission.SendMessages | Permission.EmbedLinks)]
     public class Settings : HanekawaCommandModule
     {
-        
+        private readonly CacheService _cache;
+        public Settings(CacheService cache) => _cache = cache;
+
+        [Name("Add Prefix")]
+        [Command("prefix", "addprefix")]
+        [Description("Adds a prefix to the bot, if it doesn't already exist")]
+        public async Task<DiscordCommandResult> AddPrefixAsync([Remainder] string prefix)
+        {
+            await using var db = Context.Scope.ServiceProvider.GetRequiredService<DbService>();
+            var config = await db.GetOrCreateGuildConfigAsync(Context.Guild);
+            if (config.Prefix == prefix)
+                return Reply($"{prefix} is already a prefix on this server.", HanaBaseColor.Bad());
+            
+            _cache.AddOrUpdatePrefix(Context.GuildId, new StringPrefix(prefix));
+            config.Prefix = prefix;
+            await db.SaveChangesAsync();
+            return Reply($"Added {prefix} as a prefix.", HanaBaseColor.Ok());
+        }
+
+        [Name("Set embed color")]
+        [Command("embed")]
+        [Description("Changes the embed colour of the bot")]
+        public async Task<DiscordCommandResult> SetEmbedColorAsync(Color color)
+        {
+            await Reply("Would you like to change embed color to this ? (y/n)", new Color(color));
+            var response = await Context.Bot.GetInteractivity()
+                .WaitForMessageAsync(Context.ChannelId, e => e.Member.Id == Context.Author.Id);
+            if (response == null) return Reply("Timed out...", HanaBaseColor.Bad());
+            if (response.Message.Content.ToLower() != "y" && response.Message.Content.ToLower() != "yes")
+                return Reply("Cancelled", HanaBaseColor.Bad());
+            
+            await using var db = Context.Scope.ServiceProvider.GetRequiredService<DbService>();
+            var cfg = await db.GetOrCreateGuildConfigAsync(Context.Guild);
+            _cache.AddOrUpdateColor(Context.Guild.Id, new Color(color));
+            cfg.EmbedColor = color.RawValue;
+            await db.SaveChangesAsync();
+            return Reply("Changed default embed color", HanaBaseColor.Ok());
+        }
+
+        [Name("Set embed color")]
+        [Command("embed")]
+        [Description("Changes the embed colour of the bot")]
+        public async Task<DiscordCommandResult> SetEmbedColorAsync(int r, int g, int b)
+        {
+            var color = new Color(r, g, b);
+            await Reply("Would you like to change embed color to this ? (y/n)", color);
+            var response =
+                await Context.Bot.WaitForMessageAsync(Context.ChannelId, e => e.Member.Id == Context.Author.Id);
+            if (response == null) return Reply("Timed out...", HanaBaseColor.Bad());
+            if (response.Message.Content.ToLower() != "y" && response.Message.Content.ToLower() != "yes")
+                return Reply("Canceled", HanaBaseColor.Bad());
+            
+            await using var db = Context.Scope.ServiceProvider.GetRequiredService<DbService>();
+            var cfg = await db.GetOrCreateGuildConfigAsync(Context.Guild);
+            _cache.AddOrUpdateColor(Context.Guild.Id, color);
+            cfg.EmbedColor = color.RawValue;
+            await db.SaveChangesAsync();
+            return Reply("Changed default embed color", HanaBaseColor.Ok());
+        }
     }
 }
