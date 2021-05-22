e0037046f7c8598140273eba52250ce685f097fd
Sphexator <kristoffer.tunlid@gmail.com>
2021-05-23 00:36:03 +0200

    Added webhook ids to logging cfg

--- Hanekawa.Database/Tables/Config/Guild/LoggingConfig.cs
+++ Hanekawa.Database/Tables/Config/Guild/LoggingConfig.cs
@@ -5,21 +5,33 @@ namespace Hanekawa.Database.Tables.Config.Guild
     public class LoggingConfig
     {
         public Snowflake GuildId { get; set; }
-        public Snowflake? LogJoin { get; set; } = null;
+        
+        public Snowflake? LogJoin { get; set; }
         public string WebhookJoin { get; set; }
-        public Snowflake? LogMsg { get; set; } = null;
+        public Snowflake? WebhookJoinId { get; set; }
+        
+        public Snowflake? LogMsg { get; set; }
         public string WebhookMessage { get; set; }
-        public Snowflake? LogBan { get; set; } = null;
+        public Snowflake? WebhookMessageId { get; set; }
+        
+        public Snowflake? LogBan { get; set; }
         public string WebhookBan { get; set; }
-        public Snowflake? LogAvi { get; set; } = null;
+        public Snowflake? WebhookBanId { get; set; }
+        
+        public Snowflake? LogAvi { get; set; }
         public string WebhookAvi { get; set; }
-        public Snowflake? LogWarn { get; set; } = null;
+        public Snowflake? WebhookAviId { get; set; }
+        
+        public Snowflake? LogWarn { get; set; }
         public string WebhookWarn { get; set; }
-        public Snowflake? LogAutoMod { get; set; } = null;
+        public Snowflake? WebhookWarnId { get; set; }
+        
+        public Snowflake? LogAutoMod { get; set; }
         public string WebhookAutoMod { get; set; }
-        public Snowflake? LogVoice { get; set; } = null;
+        public Snowflake? WebhookAutoModId { get; set; }
+        
+        public Snowflake? LogVoice { get; set; }
         public string WebhookVoice { get; set; }
-        public Snowflake? LogReaction { get; set; } = null;
-        public string ReactionWebhook { get; set; } = null;
+        public Snowflake? WebhookVoiceId { get; set; }
     }
 }
