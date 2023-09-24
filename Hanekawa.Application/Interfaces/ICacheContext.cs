5750e4efb2bd7ee954577f2785d3a62618dae3ef
Kristoffer T <kristoffer.tunlid@gmail.com>
2023-09-24 23:23:20 +0200

    re-added db extensions and minor cache interface implementations

--- Hanekawa.Application/Interfaces/ICacheContext.cs
+++ Hanekawa.Application/Interfaces/ICacheContext.cs
@@ -2,5 +2,8 @@
 
 public interface ICacheContext
 {
-    
+    Task<TEntity?> GetAsync<TEntity>(string key);
+    Task<TEntity?> GetAsync<TEntity>(string key, TimeSpan expiration);
+    Task AddAsync<TEntity>(string key, TEntity value);
+    Task AddAsync<TEntity>(string key, TEntity value, TimeSpan expiration);
 }
--- Hanekawa.Infrastructure/Extensions/DbExtensions.cs
+++ Hanekawa.Infrastructure/Extensions/DbExtensions.cs
@@ -1,57 +1,50 @@
 ﻿using System;
-using System.Linq;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Hanekawa.Application.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Hanekawa.Infrastructure.Extensions
 {
     public static class DbExtensions
     {
-        /*
         public static async ValueTask<TEntity> GetOrReceiveCacheAsync<TEntity>(this IServiceProvider serviceProvider,
             ulong id, TimeSpan? timeSpan = null) where TEntity : class, new()
-            => await GetOrReceiveCacheAsync<TEntity>(serviceProvider.CreateScope(), id);
+            => await GetOrReceiveCacheAsync<TEntity>(serviceProvider.CreateScope(), id, timeSpan);
 
         public static async ValueTask<TEntity> GetOrReceiveCacheAsync<TEntity>(this IServiceScope serviceScope,
             ulong key, TimeSpan? timeSpan = null) where TEntity : class, new()
         {
-            var cache = serviceScope.ServiceProvider.GetRequiredService<IRedisCacheClient>();
+            var cache = serviceScope.ServiceProvider.GetRequiredService<ICacheContext>();
             var result = timeSpan.HasValue
-                ? await cache.Db0.GetAsync<TEntity>($"{nameof(TEntity)}-{key}", timeSpan.Value)
-                : await cache.Db0.GetAsync<TEntity>($"{nameof(TEntity)}-{key}");
-            if (result != null) return result;
+                ? await cache.GetAsync<TEntity>($"{nameof(TEntity)}-{key}", timeSpan.Value)
+                : await cache.GetAsync<TEntity>($"{nameof(TEntity)}-{key}");
+            if (result is not null) return result;
             
             await using var db = serviceScope.ServiceProvider.GetRequiredService<DbService>();
             result = await GetOrCreateEntityAsync<TEntity>(db, key);
 
             if (timeSpan.HasValue)
-                await cache.Db0.AddAsync($"{nameof(TEntity)}-{key}", result, timeSpan.Value);
+                await cache.AddAsync($"{nameof(TEntity)}-{key}", result, timeSpan.Value);
             else
-                await cache.Db0.AddAsync($"{nameof(TEntity)}-{key}", result);
+                await cache.AddAsync($"{nameof(TEntity)}-{key}", result);
 
             return result;
         }
 
-        public static async ValueTask<TEntity?> GetOrCreateEntityAsync<TEntity>(this DbService context, 
+        public static async ValueTask<TEntity> GetOrCreateEntityAsync<TEntity>(this IDbContext context, 
             params ulong[] args) where TEntity : class, new()
         {
-            var config = await context.FindAsync<TEntity>(args);
-            if (config != null) return config;
+            if (context is not DbService dbService) 
+                throw new ValidationException("This IDbContext isn't inherited by DbService");
+            
+            var config = await dbService.FindAsync<TEntity>(args);
+            if (config is not null) return config;
             
             config = (TEntity)Activator.CreateInstance(typeof(TEntity), new { args })!;
-            try
-            {
-                await context.AddAsync(config);
-                await context.SaveChangesAsync();
-                return await context.FindAsync<TEntity>(args);
-            }
-            catch
-            {
-                return config;
-            }
+            await dbService.AddAsync(config);
+            await context.SaveChangesAsync();
+            return (await dbService.FindAsync<TEntity>(args))!;
         }
-        */
     }
 }
