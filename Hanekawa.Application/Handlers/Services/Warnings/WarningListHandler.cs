a76dda557ba92b0b46169f825b248a64980e4639
Kristoffer T <kristoffer.tunlid@gmail.com>
2024-03-10 22:40:39 +0100

    Added warning list handler

--- Hanekawa.Application/Handlers/Services/Warnings/WarningListHandler.cs
+++ Hanekawa.Application/Handlers/Services/Warnings/WarningListHandler.cs
@@ -1,14 +1,28 @@
-﻿using Hanekawa.Entities;
+﻿using System.Text;
+using Hanekawa.Application.Interfaces;
+using Hanekawa.Entities;
+using Hanekawa.Extensions;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hanekawa.Application.Handlers.Services.Warnings;
 
-public record WarningList(ulong GuildId, ulong? UserId) : IRequest<Response<Message>>;
+public record WarningList(ulong GuildId, ulong? UserId) : IRequest<Response<Pagination<Message>>>;
 
-public class WarningListHandler : IRequestHandler<WarningList, Response<Message>>
+public class WarningListHandler(IDbContext db ) : IRequestHandler<WarningList, Response<Pagination<Message>>>
 {
-    public Task<Response<Message>> Handle(WarningList request, CancellationToken cancellationToken)
+    public async Task<Response<Pagination<Message>>> Handle(WarningList request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var result = await db.Warnings.Where(x => 
+                x.GuildId == request.GuildId && 
+                x.UserId == request.UserId)
+            .ToArrayAsync(cancellationToken: cancellationToken);
+        if (result.Length == 0)
+        {
+            return new(new([new("No warnings found")]));
+        }
+
+        var pages = result.BuildPage().Paginate<Message>();
+        return new(new(pages));
     }
 }
