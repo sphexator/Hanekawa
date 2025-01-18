01751c150723927bbd7c3648cd7845c0888cd561
Kristoffer T <kristoffer.tunlid@gmail.com>
2025-01-18 21:39:53 +0100

    inventory things

---
+++ Hanekawa.Application/Services/InventoryService.cs
@@ -0,0 +1,19 @@
+﻿using Hanekawa.Entities.Users;
+
+namespace Hanekawa.Application.Services;
+
+public class InventoryService
+{
+
+}
+
+public interface IInventoryService
+{
+    ValueTask<GuildUser> GetInventoryAsync(ulong userId);
+    ValueTask UpdateInventoryAsync(GuildUser user);
+    ValueTask AddItemAsync(GuildUser user, Guid itemId, int amount);
+    ValueTask RemoveItemAsync(GuildUser user, Guid itemId, int amount);
+    ValueTask<bool> HasItemAsync(GuildUser user, Guid itemId, int amount);
+    ValueTask<bool> HasItemAsync(GuildUser user, Guid itemId);
+    ValueTask<int> GetItemCountAsync(ulong userId, Guid itemId);
+}
--- Hanekawa/Entities/Users/User.cs
+++ Hanekawa/Entities/Users/User.cs
@@ -1,4 +1,5 @@
-﻿using Hanekawa.Interfaces;
+﻿using System.ComponentModel.DataAnnotations;
+using Hanekawa.Interfaces;
 
 namespace Hanekawa.Entities.Users;
 
@@ -7,4 +8,47 @@ public class User : IEntity
     public ulong Id { get; set; }
     public DateTimeOffset? PremiumExpiration { get; set; }
     public List<GuildUser> GuildUsers { get; set; } = [];
+}
+
+public class Equipment : IEntity
+{
+    public ulong Id { get; set; }
+
+    public Guid BackgroundId { get; set; }
+    public Inventory Background { get; set; } = null!;
+}
+
+public class Inventory
+{
+    [Key]
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public Guid ItemId { get; set; }
+    public Item Item { get; set; } = null!;
+    public int Amount { get; set; } = 0;
+
+    public ulong UserId { get; set; }
+    public User User { get; set; } = null!;
+}
+
+public class Item
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public int Price { get; set; } = 0;
+
+    public ItemType Type { get; set; } = null!;
+    public Guid TypeId { get; set; }
+}
+
+public class ItemType
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = null!;
+    public bool Equippable { get; set; }
+    public bool Consumable { get; set; }
+    public bool Stackable { get; set; }
+    public bool Sellable { get; set; }
+    public bool Tradable { get; set; }
 }
