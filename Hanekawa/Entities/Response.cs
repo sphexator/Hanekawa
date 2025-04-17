dc898de0e75e703fe1e86f456c903ef532c82fcc
Kristoffer T <kristoffer.tunlid@gmail.com>
2025-04-18 00:34:09 +0200

    Enhance Response class to include success status and improve constructor flexibility

--- Hanekawa/Entities/Response.cs
+++ Hanekawa/Entities/Response.cs
@@ -2,6 +2,19 @@
 
 public class Response<T> where T : notnull
 {
-    public Response(T data) => Data = data;
-    public T Data { get; set; }
+    public Response(T value)
+    {
+        Value = value;
+        IsSuccess = true;
+    }
+
+    public Response(T value, bool isSuccess)
+    {
+        IsSuccess = isSuccess;
+        Value = value;
+    }
+
+    public T Value { get; set; }
+
+    public bool IsSuccess { get;  }
 }
